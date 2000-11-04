ddfe9d91c1edf4c77b07a5f5226f7517b44d2d8b
shafi <shafi@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2000-11-04 15:56:18 +0000

    improvement
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@88 f1531174-cb10-0410-9fe6-89aa7ac3eedb

--- ecell/pyecs/testPyEcs.py
+++ ecell/pyecs/testPyEcs.py
@@ -2,8 +2,19 @@
 
 import ecs
 
+print 'create Simulator instance.'
 s = ecs.Simulator()
+
+print 'makePrimitive()...'
+s.makePrimitive('Cell','System:/:CELL','The cell')
+
+print 'sendMessage()...'
+s.sendMessage( 'System:/:CELL', 'supersystem', '/' )
+
+print 'getMessage()...'
+tuple = s.getMessage( 'System:/:CELL', 'id' )
+print 'got key=%s body=%s' % (tuple[0], tuple[1])
+
+
+print 'step()...'
 s.step()
-#s.makePrimitive('Cell','/:CELL','The cell')
-#s.getMessage()
-#s.sendMessage()
