5e206563150cd745fc0d5050610568c1157f1fea
shafi <shafi@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2001-05-12 15:09:48 +0000

    changed interpreter name to /usr/bin/python2
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@172 f1531174-cb10-0410-9fe6-89aa7ac3eedb

--- ecell/pyecs/test.py
+++ ecell/pyecs/test.py
@@ -1,13 +1,13 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 
 import ecs
 
 def printProperty( sim, fqpi, propertyname ):
-    value = sim.getMessage( fqpi, propertyname )
+    value = sim.getProperty( fqpi, propertyname )
     print fqpi, '::', propertyname, '\t=\t', value
 
 def printAllProperties( sim, fqpi ):
-    properties = sim.getMessage( fqpi, 'PropertyList' )
+    properties = sim.getProperty( fqpi, 'PropertyList' )
     for property in properties:
         printProperty( sim, fqpi, property )
 
@@ -21,20 +21,20 @@ print 'create Simulator instance.'
 s = ecs.Simulator()
 
 print 'make substances...'
-s.makePrimitive('Substance','Substance:/:A','substance A')
-s.makePrimitive('Substance','Substance:/:B','substance B')
-s.makePrimitive('Substance','Substance:/:C','substance C')
+s.createEntity('Substance','Substance:/:A','substance A')
+s.createEntity('Substance','Substance:/:B','substance B')
+s.createEntity('Substance','Substance:/:C','substance C')
 
 print 'make reactors...'
-s.makePrimitive('ConstantParameterReactor','Reactor:/:RC1','constant reactor')
+s.createEntity('ConstantParameterReactor','Reactor:/:RC1','constant reactor')
 
 print 'set Substance:/:A Quantity = 30'
-s.sendMessage( 'Substance:/:A', 'Quantity', (30,) )
+s.setProperty( 'Substance:/:A', 'Quantity', (30,) )
 
 print 'initialize()...'
 s.initialize()
 
-substancelist = s.getMessage( 'System:/:/', 'SubstanceList' )
+substancelist = s.getProperty( 'System:/:/', 'SubstanceList' )
 
 printList( s, 'Substance', '/' , substancelist )
 
@@ -42,7 +42,7 @@ print
 
 printProperty( s, 'Substance:/:A', 'Quantity' )
 print 'changing Quantity of Substance:/:A...'
-s.sendMessage( 'Substance:/:A', 'Quantity', (1,) )
+s.setProperty( 'Substance:/:A', 'Quantity', (1,) )
 printProperty( s, 'Substance:/:A', 'Quantity' )
 
 print
