782447d241ad1a38bcbb0c0b568ecc02e30dba40
kem <kem@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2001-08-06 10:40:30 +0000

    First commit by kem
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@210 f1531174-cb10-0410-9fe6-89aa7ac3eedb

---
+++ osogo/backups/hello.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import string
+
+import gtk
+import gnome.ui
+import GDK
+import libglade
+
+class Window:
+
+    def __init__( self, gladefile=None, root=None ):
+        self.widgets = libglade.GladeXML( filename=gladefile, root=root )
+
+    def addHandlers( self, handlers ):
+        self.widgets.signal_autoconnect( handlers )
+        
+    def addHandler( self, name, handler, *args ):
+        self.widgets.signal_connect( name, handler, args )
+
+    def getWidget( self, key ):
+        return self.widgets.get_widget( key )
+
+    def __getitem__( self, key ):
+        return self.widgets.get_widget( key )
+
+class MainWindow(Window):
+
+    def __init__( self, gladefile ):
+
+        theHandlerMap = { 'on_button1_clicked': self.button1_clicked }
+
+        Window.__init__( self, gladefile )
+        self.addHandlers( theHandlerMap )
+
+    def button1_clicked( self,obj ):
+        print 'hello!'
+
+def mainLoop():
+    # FIXME: should be a custom function
+    gtk.mainloop()
+
+def main():
+    aWindow = MainWindow( 'hello.glade' )
+    mainLoop()
+
+if __name__ == "__main__":
+    main()
+
