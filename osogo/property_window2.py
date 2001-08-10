eefadd7728c204bb38342ae4aee8d1a1eea7a40b
beijing <beijing@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2001-08-10 11:14:36 +0000

    ugokanai
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@216 f1531174-cb10-0410-9fe6-89aa7ac3eedb

---
+++ osogo/property_window2.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+
+import string
+
+import gtk
+import gnome.ui
+import GDK
+import libglade
+
+
+
+class Window:
+
+    def __init__( self, gladefile=None, root=None ):
+        self.widgets = libglade.GladeXML( filename=gladefile, root=root )
+
+    def addHandlers( self, handlers ):
+        self.widgets.signal_autoconnect( handlers )
+        
+    def addHandler( self, name, handler, *args ):
+        self.widgets.signal_connect( name, handler, args )
+
+    def getWidget( self, key ):
+        return self.widgets.get_widget( key )
+
+    def __getitem__( self, key ):
+        return self.widgets.get_widget( key )
+
+class MainWindow(Window):
+
+    def __init__( self, gladefile ):
+
+        self.theHandlerMap = {
+            }
+        
+        Window.__init__( self, gladefile )
+
+        self.addHandlers( self.theHandlerMap)
+         
+        self.theProperty_List = self.getWidget( "clist1" )
+        self.theProperty_Name= self.getWidget( "label3" )
+
+
+#    def setName( self ):
+#        MainWindow.theProperty_entity.set_text('EntytyName')
+#    setName( self )
+       
+    def update( self ):
+        aPropertyList = tmpget( 'PropertyList' )
+        for x in aPropertyList:
+            aValueList = tmpget( x )
+            aName = aValueList[0]
+            aValueList = aValueList[1:]
+#            self.theProperty_List.append( aValueList )
+            print aValueList
+
+def mainQuit( obj, data ):
+    print obj,data
+    gtk.mainquit()
+
+def mainLoop():
+    # FIXME: should be a custom function
+    gtk.mainloop()
+
+def main():
+    aMainWindow = MainWindow( 'property_window.glade' )
+    aMainWindow.addHandler( 'gtk_main_quit', mainQuit )    
+    aMainWindow.update()
+    mainLoop()
+    
+testdic={ 'PropertyList': ('PropertyList', 'A','B','C'),
+          'A': ('aaa',) ,'B': (1.04E-3,) ,'C': (41,) }
+
+def tmpget( name ):
+    aList = list(testdic[name])
+    aList.insert( 0, name )
+    return tuple( aList )
+
+if __name__ == "__main__":
+
+    main()
+    
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
