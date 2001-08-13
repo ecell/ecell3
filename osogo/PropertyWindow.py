759f37575b30b6025e3a0d9d00c3c50726637170
ducati <ducati@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2001-08-13 07:48:17 +0000

    hello
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@238 f1531174-cb10-0410-9fe6-89aa7ac3eedb

--- osogo/PropertyWindow.py
+++ osogo/PropertyWindow.py
@@ -29,9 +29,9 @@ class Window:
 
 class MainWindow(Window):
 
-
+    
     def __init__( self, gladefile ):
-
+        
         self.theHandlerMap = {
             }
         
@@ -44,15 +44,15 @@ class MainWindow(Window):
         self.theIDEntry = self.getWidget( "entry_ID" )
         self.thePathEntry = self.getWidget( "entry_PATH" )
         self.theNameEntry = self.getWidget( "entry_NAME" )
-
-
+        
+        
         
         #    def setName( self ):
         #        MainWindow.thePropertyEntity.set_text('EntytyName')
         #    setName( self )
         
-
-              
+        
+        
     def update( self ):
         aPropertyList = list( tmpget( 'PropertyList' ) )
         bPropertyList = testdic['Name'][0]
@@ -68,31 +68,44 @@ class MainWindow(Window):
         #        self.thePropertyListA.clear()
         #        self.thePropertyListB.clear()
         #        self.thePropertyListC.clear()
-
+        
         
         #        print FQPPList[0]
         
-#        aFQPP = getFQPP()
-
+        #        aFQPP = getFQPP()
+        
         self.theTypeEntry.set_text(toString(FQPPList.getType()))
         self.theIDEntry.set_text(toString(FQPPList.getID()))
         self.thePathEntry.set_text(toString(FQPPList.getSystemPath()))
         self.theNameEntry.set_text(bPropertyList)
-
         
-
+        
+        
         for x in aPropertyList:
-            aValueList = tmpget( x )
-            #            aName = aValueList[0]
-            #            aValueList = aValueList[1:]
+            if (x=='Substrate' or x=='Product') :
+                
+                for y in x:
 
-            aValueList = map( toString, aValueList )
-            
-            self.thePropertyClist.append( aValueList )
-
-            
-            #            print aValueList
-            
+                    aValueList = list( tmpget( y ) )
+                    #aValueList = tmpget( y )
+                    #aValueList = tmpget( x )
+                    
+                    #            aName = aValueList[0]
+                    #            aValueList = aValueList[1:]
+                    
+                    aValueList = map( toString, aValueList )
+                    
+                    self.thePropertyClist.append( aValueList) 
+                    
+                    
+                    #            print aValueList
+                    
+                else:
+                    aValueList = tmpget( x )
+                    aValueList = map( toString, aValueList )
+                    
+                    self.thePropertyClist.append( aValueList )
+                
 
 def toString( object ):
     return str( object )
