36ff50966e015348d38f4eeb2b4e2f30f58ec55c
kem <kem@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2001-08-14 04:56:29 +0000

    commited by kem
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@247 f1531174-cb10-0410-9fe6-89aa7ac3eedb

--- osogo/NumericWindow.py
+++ osogo/NumericWindow.py
@@ -1,56 +1,57 @@
 #!/usr/bin/env python
 
+from ecellgui import *
 import string
 
-import gtk
-import gnome.ui
-import GDK
-import libglade
+#TARGET_STRING = 0
+#TARGET_ROOTWIN = 1
 
-from Window import *
+#target = [ ('STRING', 0, TARGET_STRING),
+#           ('text/plain', 0, TARGET_STRING),
+#           ('application/x-rootwin-drop', 0, TARGET_ROOTWIN) ]
 
+class NumericWindow( Interface ):
 
-class NumericWindow(Window):
+    def __init__( self,fqpps ):
+        self.theGladeFileName = 'NumericWindow.glade'
+        Interface.__init__( self, self.theGladeFileName, fqpps )
+        self.addHandler('input', self.inputValue)
+        self.initInterface( fqpps )
+ #       self["fqpp_label"].drag_dest_set( gtk.DEST_DEFAULT_ALL, target, GDK.ACTION_COPY)
+#        self["fqpp_label"].drag_source_set( GDK.BUTTON1_MASK, target, GDK.ACTION_COPY|GDK.ACTION_MOVE )
 
-    def __init__( self, gladefile ):
+    def initInterface( self, fqpps ):
+        self.theFQPPList = string.split(fqpps, ':')
+        self.theIDProperty = self.theFQPPList[2]
+        self["fqpp_label"].set_text(self.theIDProperty)
+        self["spinbutton"].set_text('1.00000')
 
-        self.theHandlerMap = {'input':           self.input}
-
-        Window.__init__( self, gladefile )
-        self.addHandlers( self.theHandlerMap )
-        
-    def setText( self, name , text ):
-        obj = self.getWidget(name)
-        obj.set_text(text)
-        
-    def setLabel(self,name,text):
-        obj = self.getWidget(name)
-        obj.set_label(text)
-
-    def input( self,obj ):
+    def inputValue( self, obj, n ):
         aNumberString =  obj.get_text()
         aNumber = string.atof( aNumberString )
         print aNumberString
 
+### test code
+
+def mainLoop():
+
+    aFQPP1 = 'Substance:/CELL/CYTOPLASM:ATP|Quantity'
+    aWindow1 = NumericWindow( aFQPP1 )
+    aFQPP2 = 'Substance:/CELL/CYTOPLASM:ADP|Quantity'
+    aWindow2 = NumericWindow( aFQPP2 )
+
+#    aWindow2 = NumericWindow( 'Substance:/CELL/CYTOPLASM:ATP' )
+#    aWindow3 = NumericWindow( 'Reactor:/CELL/CYTOPLASM:ATPase-0' )
+#    aWindow4 = NumericWindow( 'System:/CELL:CYTOPLASM' )
+#    aWindow5 = NumericWindow( 'Substance:/CELL/CYTOPLASM:ATP|Quantity,Substance:/CELL/CYTOPLASM:ADP|Quantity' )
+#    aWindow6 = NumericWindow( 'Substance:/CELL/CYTOPLASM:ATP|Quantity,Substance:/CELL/CYTOPLASM:ATPase' )
+    
+    gtk.mainloop()
+
+def main():
+    mainLoop()
 
 if __name__ == "__main__":
-
-    from main import *
-
-    def main():
-        systemPath = '/CELL/CYTOPLASM'
-        ID = 'ATP'
-        FQPI = systemPath + ':' + ID  
-        propertyName = 'quantity'
-        propertyValue = '0.00000'
-        aWindow = NumericWindow( 'NumericWindow.glade' )
-        aWindow.addHandler( 'gtk_main_quit', mainQuit )
-        aWindow.setText("spinbutton1", propertyValue)
-        aWindow.setText("label1",ID)  
-        aWindow.setLabel("frame1", propertyName)
-        mainLoop()
-
-
     main()
 
 
