77b8942379b628bca0345fcc5b617ea159286b14
kem <kem@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2001-08-16 09:38:06 +0000

    PaletteWindow can be opened from MainWindow. it dosen't be opened by itself.
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@280 f1531174-cb10-0410-9fe6-89aa7ac3eedb

--- osogo/PaletteWindow.py
+++ osogo/PaletteWindow.py
@@ -8,29 +8,27 @@ import string
 
 class PaletteWindow(GtkWindow):
     def __init__( self ):
-        GtkWindow.__init__(self, WINDOW_TOPLEVEL)
+        GtkWindow.__init__( self, WINDOW_TOPLEVEL )
 
-        self.theToolbar = GtkToolbar(ORIENTATION_VERTICAL, TOOLBAR_BOTH )
-        self.add(self.theToolbar)
+        aToolbar = GtkToolbar( ORIENTATION_VERTICAL, TOOLBAR_BOTH )
+        self.add( aToolbar )
 
         aIndicator = 0
-        for aFileName in os.listdir('plugins') :
+        for aFileName in os.listdir( 'plugins' ) :
 
-            if re.search('\.py$', aFileName):
-                aModuleName = string.replace(aFileName, '.py', '')
+            if re.search( '\.py$', aFileName ):
+                aModuleName = string.replace( aFileName, '.py', '' )
 
                 if aIndicator == 0:
                     aIndicator = 1
 
-                    aFirstButton = GtkRadioButton(label = aModuleName)
-                    self.theToolbar.append_widget( aFirstButton, '', '' )
+                    aFirstButton = GtkRadioButton( label = aModuleName )
+                    aToolbar.append_widget( aFirstButton, '', '' )
 
                 else :
-                    aButton = GtkRadioButton(aFirstButton, label = aModuleName)
-                    self.theToolbar.append_widget( aButton, '', '' )
+                    aButton = GtkRadioButton( aFirstButton, label = aModuleName )
+                    aToolbar.append_widget( aButton, '', '' )
             
-        self.show_all()
-
 def mainLoop():
     aPaletteWindow = PaletteWindow()
     gtk.mainloop()
