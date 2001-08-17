e01f1779c5bfd528b78a5f217b49abde8330dafd
moto <moto@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2001-08-17 13:14:58 +0000

    *** empty log message ***
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@310 f1531174-cb10-0410-9fe6-89aa7ac3eedb

--- osogo/plugins/ProgressbarWindow.py
+++ osogo/plugins/ProgressbarWindow.py
@@ -2,71 +2,80 @@
 
 import string
 
-import gtk
-import gnome.ui
-import GDK
-import libglade
+from Plugin import *
+from ecssupport import *
+
 import Numeric
 
+class ProgressbarWindow(PluginWindow):
+    
+    def __init__( self, dirname, sim, data ):
 
-class Window:
-
-    def __init__( self, gladefile=None, root=None ):
-        self.widgets = libglade.GladeXML( filename=gladefile, root=root )
-
-    def addHandlers( self, handlers ):
-        self.widgets.signal_autoconnect( handlers )
-        
-    def addHandler( self, name, handler, *args ):
-        self.widgets.signal_connect( name, handler, args )
-
-    def getWidget( self, key ):
-        return self.widgets.get_widget( key )
-
-    def __getitem__( self, key ):
-        return self.widgets.get_widget( key )
-
-
-class MainWindow(Window):
-
-    def __init__( self, gladefile ):
-
+        PluginWindow.__init__( self, dirname, sim, data )
         self.thePositiveFlag = 1
         self.theAutoChangeFlag = 1
-
         self.theActualValue = 0
         self.theBarLength = 0
         self.theMultiplier = 0
+        
+        self.addHandlers( { \
+            'level_spinbutton_activate': self.updateBySpinbutton,
+            'level_spinbutton_changed': self.updateBySpinbutton,
+            'auto_button_toggled': self.updateByAutoButton ,
+#            'on_entry1_activate': self.changeValueFromEntryWindow
+            })
+        # self['property_id_label'].set_text(self.theID)
+        self.theIDEntry = self.getWidget( "property_id_label" )
+        self.theFPN = data[0]
+        self.initialize()
+        
+    def initialize( self ):
 
-        self.theHandlerMap = {
-                              'level_spinbutton_activate': self.updateBySpinbutton,
-                              'level_spinbutton_changed': self.updateBySpinbutton,
-                              'auto_button_toggled': self.updateByAutoButton
-                              }
+        self.theSelected = ''
+        
+        self.theFullID = convertToFullID( self.theFPN )
+        self.theType = str( self.theFullID[TYPE] )
+        self.theID   = str( self.theFullID[ID] )
+        self.thePath = str( self.theFullID[SYSTEMPATH] )
 
-        Window.__init__( self, gladefile )
-        self.addHandlers( self.theHandlerMap )
+        self.update()
+        
+    def update( self ):
+        self.theIDEntry.set_text  ( self.theID )
+        aValue = self.theSimulator.getProperty( self.theFPN )
+        value = aValue[0]
+#        value = 124143.807
+        self.theActualValue = value
+        self.theBarLength , self.theMultiplier , self.thePositiveFlag \
+                          = self.calculateBarLength( value )
+
+        # aIndicator = self.theBarLength * self.thePositiveFlag
+        aIndicator = (value / (float)(10**(self.theMultiplier))) \
+                     * self.thePositiveFlag
+        
+        self['progressbar'].set_value(int(self.theBarLength))
+        self['progressbar'].set_format_string(str(aIndicator))
+        self['level_spinbutton'].set_value(self.theMultiplier)
 
     def updateByAuto( self, value ):
         self.theAutoChangeFlag = 1
-        self.update( value )
+        self.update()
 
     def updateBySpinbutton( self, spinbutton_obj ):
         if self.theAutoChangeFlag :
             pass
         else :
             self['auto_button'].set_active( 0 )
-        self.update( self.theActualValue )
+        self.update()
 
         aNumberString =  spinbutton_obj.get_text()
         aNumber = string.atof( aNumberString )
         self['level_spinbutton'].set_value(aNumber)
-        # value = propertyValue1
 
         self.theAutoChangeFlag = 0
 
     def updateByAutoButton(self, autobutton):
-        self.update( self.theActualValue)
+        self.update()
 
     def calculateBarLength( self, value ):
         if value < 0 :
@@ -80,66 +89,54 @@ class MainWindow(Window):
         else :
             aMultiplier = self['level_spinbutton'].get_value()
 
-#        aBarLength = (value / (float)(10**(aMultiplier)))
+        # aBarLength = (value / (float)(10**(aMultiplier)))
         aBarLength = (Numeric.log10(value)-aMultiplier+1)*10/3
 
         return  aBarLength, aMultiplier, aPositiveFlag
-        
-    def update( self, value ):
-        self.theActualValue = value
-        self.theBarLength , self.theMultiplier , self.thePositiveFlag \
-                          = self.calculateBarLength( value )
-
-#        aIndicator = self.theBarLength * self.thePositiveFlag
-        aIndicator = (value / (float)(10**(self.theMultiplier))) \
-                     * self.thePositiveFlag
-        
-        self['progressbar'].set_value(int(self.theBarLength))
-        self['progressbar'].set_format_string(str(aIndicator))
-        self['level_spinbutton'].set_value(self.theMultiplier)
-        
-    ### for test
-    def changeValueFromEntryWindow( self, obj, a):
-        
-        aValueString = obj.get_text()
-        aValue = string.atof( aValueString )
-        print aValue
-        self.changeValue( aValue )
+                
+    ####### for test #############################
+    #def changeValueFromEntryWindow( self, obj, a):
+    #    
+    #    aValueString = obj.get_text()
+    #    aValue = string.atof( aValueString )
+    #    print aValue
+    #    self.changeValue( aValue )
+    #
+    #############################################
 
     def changeValue( self, value ):
         self.updateByAuto( value )
-
-def mainQuit( obj, data ):
-    print obj,data
-    gtk.mainquit()
-
-def mainLoop():
-    # FIXME: should be a custom function
-    gtk.mainloop()
-
-def main():
-    systemPath = '/CELL/CYTOPLASM'
-    #    ID = 'ATPase: Activity'
-    ID = 'ATPase'
-    FQPI = systemPath + ':' + ID  
     
-    aWindow = MainWindow( 'ProgressbarWindow.glade' )
-
-    aWindow.addHandler( 'gtk_main_quit', mainQuit )
-    aWindow.addHandler( 'on_entry1_activate', aWindow.changeValueFromEntryWindow)
-
-    aWindow['property_id_label'].set_text(ID)
-
-    aWindow.update(propertyValue1)
-
-    # aWindow.setAuto("auto_button")
-    #    aWindow.setLabel("frame1", propertyName)
-    
-    mainLoop()
 
 if __name__ == "__main__":
-    
-    propertyValue1 = -750.0000
+
+    class simulator:
+
+        dic={('Substance','/CELL/CYTOPLASM','ATP','quantity') : (1950,),}
+        
+        def getProperty( self, fpn ):
+            return simulator.dic[fpn]
+        
+        def setProperty( self, fpn, value ):
+            simulator.dic[fpn] = value
+
+
+    fpn = ('Substance','/CELL/CYTOPLASM','ATP','quantity')
+
+    def mainQuit( obj, data ):
+        print obj,data
+        gtk.mainquit()
+
+    def mainLoop():
+        # FIXME: should be a custom function
+        gtk.mainloop()
+
+    def main():
+        aProgressbarWindow = ProgressbarWindow( 'plugins', simulator(), [fpn,] )
+        aProgressbarWindow.addHandler( 'gtk_main_quit', mainQuit )
+        
+        mainLoop()
+
+
+    # propertyValue1 = -750.0000
     main()
-
-
