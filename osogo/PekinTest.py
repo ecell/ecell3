cce35d4ab49ff9b853711f3d44dd430444582b13
beijing <beijing@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2001-08-17 04:15:14 +0000

    for Test of EntryListWindow
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@284 f1531174-cb10-0410-9fe6-89aa7ac3eedb

---
+++ osogo/PekinTest.py
@@ -0,0 +1,16 @@
+testdic = {
+    'CELL': ('A', 'B', 'C'),
+    'ENVIRONMENT':('ENV_A', 'ENV_B', 'A')
+    }
+
+#testdic = {
+#    'System':
+#    {'CELL':
+#    {'CYTOPLASM':
+#     {'Substance': {'A', 'B', 'C'},
+#      'Reactor': {'R1', 'R2'}}
+#     {'ENVIRONMENT':
+#    {'Property': {'volume': 1.4e-15}}
+#    }
+#    }
+#    }
