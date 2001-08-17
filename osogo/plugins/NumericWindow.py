2760bad287cb7a5d49bfea1f5476950892797fb5
t98839tf <t98839tf@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2001-08-17 08:12:44 +0000

    thanks for zhaiteng ...taro
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@296 f1531174-cb10-0410-9fe6-89aa7ac3eedb

--- osogo/plugins/NumericWindow.py
+++ osogo/plugins/NumericWindow.py
@@ -1,69 +1,110 @@
 #!/usr/bin/env python
 
-import sys
-sys.path.append('..')
-sys.path.append('/home/ecell/ecell3/ecell/pyecs')
-from ViewWindow import *
+
+from Plugin import *
 from ecssupport import *
-import ecs
 
-class NumericWindow( ViewWindow ):
+class NumericWindow( PluginWindow ):
 
-    def __init__( self, fpn, sim ):
-        self.sim = sim
-        self.theGladeFileName = 'NumericWindow.glade'
-        ViewWindow.__init__( self, self.theGladeFileName, fpn )
-        self.addHandler('input_value', self.inputValue)
-        self.addHandler('increase_value', self.increaseValue)
-        self.addHandler('decrease_value', self.decreaseValue)
-        self.initInterface( fpn )
+    def __init__( self, dirname, sim, data ):
+
+        PluginWindow.__init__( self, dirname, sim, data )
+
+        self.addHandlers( { 'input_value'    :self.inputValue,
+                            'increase_value' :self.increaseValue,
+                            'decrease_value' :self.decreaseValue } )
+
+        self.theFPN = data
+        self.initInterface(self.theFPN)
+        
+
+    def initialize( self ):
+
+        self.theFullID = convertToFullID( self.theFPN )
+        self.theType = str( self.theFullID[TYPE] )
+        self.theID   = str( self.theFullID[ID] )
+        self.thePath = str( self.theFullID[SYSTEMPATH] )
+        
+#        self.update()
 
     def initInterface( self, fpn ):
-        self.fpnobj = FullPropertyName(fpn)
-        self["id_label"].set_text(self.fpnobj['ID'])
-        value = self.sim.getProperty( self.fpnobj['FullID'], self.fpnobj['PropertyName'])
+
+        self.theFPN = fpn
+        self.initialize()
+        self["id_label"].set_text( self.theID )
+        value = self.theSimulator.getProperty( self.theFPN )
         self.theCurValue = value[0]
         self["value_frame"].set_text(str(self.theCurValue))
 
-    def inputValue( self, obj, n ):
+        
+    def update( self ):
+
+        pass
+
+
+    def inputValue( self, obj ):
 
         aNumberString =  obj.get_text()
         self.theCurValue = string.atof( aNumberString )
+        self.changeValue()
+
+    def increaseValue( self, value ):
+
+        self.theCurValue = self.theCurValue * 2.0
+        self["value_frame"].set_text(str(self.theCurValue))
+        self.changeValue()
+
+    def decreaseValue( self, obj ):
+
+        self.theCurValue = self.theCurValue * 0.5
+        self["value_frame"].set_text(str(self.theCurValue))
+        self.changeValue()
+
+    def changeValue( self ):
+
         value = (self.theCurValue,)
-        self.sim.setProperty( self.fpnobj['fullID'], self.fpnobj['PropertyName'], value )
-        print self.sim.getProperty( self.fpnobj['FullID'], self.fpnobj['PropertyName'])
-
-    def increaseValue( self, value, n ):
-        self.theCurValue = self.theCurValue * 2
-        self["value_frame"].set_text(str(self.theCurValue))
-
-    def decreaseValue( self, obj, n ):
-        self.theCurValue = self.theCurValue / 2
-        self["value_frame"].set_text(str(self.theCurValue))
+        self.theSimulator.setProperty(self.theFPN, value)
+        print self.theSimulator.getProperty(self.theFPN)
 
 
 ### test code
 
-def mainLoop():
-
-    s = ecs.Simulator()
-
-    s.createEntity('Substance','Substance:/:ATP','ATP')
-    s.setProperty( 'Substance:/:ATP', 'Quantity', (30,) )
-    s.initialize()
-
-    aFPN1 = 'Substance:/:ATP:Quantity'
-    aWindow1 = NumericWindow( aFPN1 , s)
-#    aFPN2 = 'Substance:/:ADP:Quantity'
-#    aWindow2 = NumericWindow( aFPN1 , s)
-
-    gtk.mainloop()
-
-def main():
-    mainLoop()
-
 if __name__ == "__main__":
+
+    class simulator:
+
+        dic={('Substance','/CELL/CYTOPLASM','ATP','quantity') : (1950,),}
+
+        def getProperty( self, fpn ):
+            return simulator.dic[fpn]
+
+        def setProperty( self, fpn, value ):
+            simulator.dic[fpn] = value
+
+
+    fpn = ('Substance','/CELL/CYTOPLASM','ATP','quantity')
+
+    def mainQuit( obj, data ):
+        print obj,data
+        gtk.mainquit()
+        
+    def mainLoop():
+        # FIXME: should be a custom function
+
+        gtk.mainloop()
+
+    def main():
+        aNumericWindow = NumericWindow( 'plugins', simulator(), fpn )
+        aNumericWindow.addHandler( 'gtk_main_quit', mainQuit )
+        aNumericWindow.update()
+
+        mainLoop()
+
     main()
+
+
+
+
 
 
 
