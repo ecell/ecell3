84be5a17ac1f84b6ccec4ae70aaf5f62de04d921
beijing <beijing@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2001-08-17 06:55:24 +0000

    ECSppoiDIC
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@289 f1531174-cb10-0410-9fe6-89aa7ac3eedb

--- osogo/PekinTest.py
+++ osogo/PekinTest.py
@@ -1,16 +1,20 @@
-testdic = {
+TestDic = {
     'CELL': ('A', 'B', 'C'),
     'ENVIRONMENT':('ENV_A', 'ENV_B', 'A')
     }
 
-#testdic = {
-#    'System':
-#    {'CELL':
-#    {'CYTOPLASM':
-#     {'Substance': {'A', 'B', 'C'},
-#      'Reactor': {'R1', 'R2'}}
-#     {'ENVIRONMENT':
-#    {'Property': {'volume': 1.4e-15}}
-#    }
-#    }
-#    }
+EcsTestDic = {
+    '/':
+    {'CELL':
+     {'CYTOPLASM':
+      {'CHAOS': {'Substance':('aa', 'hai', 'B'), 'Reactor':('Rmt1','R2'), 'Property': {'Volume':1.4e-19}},
+       'Substance':('A', 'B', 'C'),
+       'Reactor': ('R1', 'R2', 'A'),
+       'Property': {'Volume': 1.4e-15}},
+      'MEMBRANE':{'Property': {'Potential': 20, 'OsmoPressure': 14},
+                  'Reactor':('Transporter1', 'Transporter2')},
+      'Property': {'pekin': 'chocobo'}},
+     'ENVIRONMENT': {'Property': {'Volume': 1.4e-12, 'Temparature': 289},
+                      'Substance': ('Glucose', 'Na', 'K')},
+    }
+    }
