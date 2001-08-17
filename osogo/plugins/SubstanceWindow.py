5c37e6828cf8a6defaa280392b08579a4357b16c
ducati <ducati@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2001-08-17 10:10:55 +0000

    *** empty log message ***
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@305 f1531174-cb10-0410-9fe6-89aa7ac3eedb

--- osogo/plugins/SubstanceWindow.py
+++ osogo/plugins/SubstanceWindow.py
@@ -1,89 +1,146 @@
 #!/usr/bin/env python
 
 import string
-import ecs
-import gtk
-import gnome.ui
-import GDK
-import libglade
-import propertyname
 
-class Window:
+from Plugin import *
+from ecssupport import *
 
-    def __init__( self, gladefile=None, root=None ):
-        self.widgets = libglade.GladeXML( filename=gladefile, root=root )
+class SubstanceWindow(PluginWindow):
 
-    def addHandlers( self, handlers ):
-        self.widgets.signal_autoconnect( handlers )
+    def __init__( self, dirname, sim, data ):
         
-    def addHandler( self, name, handler, *args ):
-        self.widgets.signal_connect( name, handler, args )
+        # 0 : not fixed  1: fixed
+        self.theFixFlug = 0
 
-    def getWidget( self, key ):
-        return self.widgets.get_widget( key )
-
-    def __getitem__( self, key ):
-        return self.widgets.get_widget( key )
-
-class MainWindow(Window):
-
-    def __init__( self, gladefile ):
-
-        self.theHandlerMap = {'button_toggled': self.fix_mode,
+        PluginWindow.__init__( self,dirname,sim, data )
+        
+        self.addHandlers( {'button_toggled': self.fix_mode,
                               'qty_increase_pressed': self.increaseQuantity, 
-                              'qty_decrease_pressed': self.decreaseQuantity ,
+                              'qty_decrease_pressed': self.decreaseQuantity,
                               'concentration_increase_pressed': self.increaseConcentration,
-                              'concentration_decrease_pressed': self.decreaseConcentration,                              }
+                              'concentration_decrease_pressed': self.decreaseConcentration,
+                             'input_quantity': self.inputQuantity,
+                             'input_concentration': self.inputConcentration
+                             } )
+
+        self.theFullPropertyName = data[0]
+        self.initialize( self.theFullPropertyName)
         
-        Window.__init__( self, gladefile )
-        self['label86'].set_text( str( theFullID ) )
-        self['Quantity_entry'].set_text( str( theQuantity ) )
-        self['Concentration_entry'].set_text( str( theConcentration ) )
+    def initialize( self, fpn ):
+        
+        self.theFullID = convertToFullID( fpn )
+        self.theFPNQuantity = tuple(convertToFullPropertyName( self.theFullID, 'quantity' ))
+        self.theFPNConcentration = tuple(convertToFullPropertyName( self.theFullID, 'concentration' ))
+        self.theType = str( self.theFullID[TYPE] )
+        self.theID   = str( self.theFullID[ID] )
+        self.thePath = str( self.theFullID[SYSTEMPATH] )
+        
+        aFullIDString = constructFullIDString( self.theFullID )
+        self["id_label"].set_text( aFullIDString )
 
-        self.addHandlers( self.theHandlerMap )
+        qvalue = self.theSimulator.getProperty( self.theFPNQuantity )
+        self.theQuantity = qvalue[0]
+        self['Quantity_entry'].set_text( str( self.theQuantity ) )
 
+        self.theConcentration = self.theSimulator.getProperty( self.theFPNConcentration )[0]
+        self['Concentration_entry'].set_text( str( self.theConcentration ) )
+
+        #self.update()
+
+    def update( self ):
+        pass
+    
     def fix_mode( self, a ) :
-        print 'hello'
+
+        self.theFixFlug = 1 - self.theFixFlug
+        if self.theFixFlug == 0:
+            print 'not fixed'
+        else:
+            print 'fixed'
+
+    def inputQuantity( self, obj ):
+
+        aNumberString = obj.get_text()
+        self.theQuantity = string.atof( aNumberString )
+        self.changeQuantity()
+
+    def inputConcentration( self, obj ):
+        
+        aNumberString = obj.get_text()
+        self.theConcentration = string.atof( aNumberString )
+        self.changeConcentration()
+    
+    def increaseQuantity( self, button_object ):
+
+        self.theQuantity *= 2.0
+        self[ "Quantity_entry" ].set_text(str(self.theQuantity))
+        self.changeQuantity()
 
     def increaseConcentration( self, button_object ):
-        self.increaseValue('Concentration_entry')
 
-    def increaseQuantity( self, button_object ):
-        self.increaseValue('Quantity_entry')
-
-    def decreaseConcentration( self, button_object ):
-        self.decreaseValue('Concentration_entry')
+        self.theConcentration *= 2.0
+        self[ "Concentration_entry" ].set_text(str(self.theConcentration))
+        self.changeConcentration()
 
     def decreaseQuantity( self, button_object ):
-        self.decreaseValue('Quantity_entry')
 
-    def increaseValue( self, name ):
-        aValue = self[ name ].get_text()
-        aNewValue = string.atof(aValue) * 2
-        self[ name ].set_text( str( aNewValue ) )
+        self.theQuantity *= 0.5
+        self[ "Quantity_entry" ].set_text(str(self.theQuantity))
+        self.changeQuantity()
 
-    def decreaseValue( self, name ):
-        aValue = self[ name ].get_text()
-        aNewValue = string.atof(aValue) / 2
-        self[ name ].set_text( str( aNewValue ) )
+    def decreaseConcentration( self, button_object ):
+
+        self.theConcentration *= 0.5
+        self[ "Concentration_entry" ].set_text(str(self.theConcentration))
+        self.changeConcentration()
+
+    def changeQuantity( self ):
+        
+        value = (self.theQuantity,)
+        self.theSimulator.setProperty(self.theFPNQuantity, value)
+        print self.theSimulator.getProperty(self.theFPNQuantity)
+
+    def changeConcentration( self ):
+        value = (self.theConcentration,)
+        self.theSimulator.setProperty(self.theFPNConcentration, value)
+        print self.theSimulator.getProperty(self.theFPNConcentration)
 
 def mainLoop():
     # FIXME: should be a custom function
     gtk.mainloop()
 
-def main():
-    aWindow = MainWindow( 'SubstanceWindow.glade' )
 
-
-    mainLoop()
 
 if __name__ == "__main__":
 
-    theQuantity = 100
-    theConcentration = 0.0145
-    theVolume = 1e-18
-    theFullID = 'Substance:/CELL/CYTOPLASM:ATP'
-    theAvogadoroNumber = 6.022 * 1e-23
+    class simulator:        
+        dic={('Substance','/CELL/CYTOPLASM','ATP','quantity') : (1950,),
+             ('Substance','/CELL/CYTOPLASM','ATP','concentration') : (0.353,),}
+        def getProperty( self, fpn ):
+            return simulator.dic[fpn]
+        
+        def setProperty( self, fpn, value ):
+            simulator.dic[fpn] = value
+            
+            
+    fpn = ('Substance','/CELL/CYTOPLASM','ATP','')
+
+
+    def mainQuit( obj, data ):
+        print obj,data
+        gtk.mainquit()
+         
+    def mainLoop():
+        # FIXME: should be a custom function
+        
+        gtk.mainloop()
+
+    def main():
+        aSubstanceWindow = SubstanceWindow( 'plugins', simulator(), [fpn,])
+        aSubstanceWindow.addHandler( 'gtk_main_quit' , mainQuit )
+        aSubstanceWindow.update()
+
+        mainLoop()
 
     main()
     
