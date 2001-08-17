6b966fbeb6505312591be6151535793d15e10db5
beijing <beijing@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2001-08-17 09:08:37 +0000

    korede dekirebane
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@301 f1531174-cb10-0410-9fe6-89aa7ac3eedb

--- osogo/backups/PekinTest2.py
+++ osogo/backups/PekinTest2.py
@@ -1,1 +1,22 @@
-testdic = {'System': ('CELL', 'ENVIRONMENT', 'CHAOS')
+TestDic = {
+    'System': {'CELL':('P','M'), 'ENVIRONMENT':'C', 'CHAOS': ('MIYOSHI','NUMATA')}}
+
+
+EcsTestDic = {
+    'System':
+    {'CELL':
+     {'System':
+      {'CYTOPLASM':
+       {'System': {'CHAOS': {'Substance':('aa', 'hai', 'B')}, 'Reactor':('Rmt1','R2'), 'Property': {'Volume':1.4e-19}},
+        'Substance':('A', 'B', 'C'),
+        'Reactor': ('R1', 'R2', 'A'),
+        'Property': {'Volume': 1.4e-15}},
+       'MEMBRANE':{'Property': {'Potential': 20, 'OsmoPressure': 14},
+                   'Reactor':('Transporter1', 'Transporter2')},
+       'Property': {'pekin': 'chocobo'}}},
+      'ENVIRONMENT': {'Property': {'Volume': 1.4e-12, 'Temparature': 289},
+                      'Substance': ('Glucose', 'Na', 'K')}
+     }
+    }
+
+
