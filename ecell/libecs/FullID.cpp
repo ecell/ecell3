b7023eca2c904e2b9e277293be1d5f74d42af859
shafi <shafi@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2001-08-18 19:17:22 +0000

    added replace FQPI
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@316 f1531174-cb10-0410-9fe6-89aa7ac3eedb

---
+++ ecell/libecs/FullID.cpp
@@ -0,0 +1,320 @@
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//
+//        This file is part of E-CELL Simulation Environment package
+//
+//                Copyright (C) 1996-2000 Keio University
+//
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//
+//
+// E-CELL is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+// 
+// E-CELL is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public
+// License along with E-CELL -- see the file COPYING.
+// If not, write to the Free Software Foundation, Inc.,
+// 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+// 
+//END_HEADER
+//
+// written by Kouichi Takahashi <shafi@e-cell.org> at
+// E-CELL Project, Lab. for Bioinformatics, Keio University.
+//
+
+#include <string>
+
+#include "Util.hpp"
+#include "Exceptions.hpp"
+
+#include "FullID.hpp"
+
+namespace libecs
+{
+
+  ///////////////////////  SystemPath
+
+  void SystemPath::parse( StringCref systempathstring )
+  {
+    if( systempathstring == "" )
+      {
+	return;
+      }
+
+    String aString( systempathstring );
+    eraseWhiteSpaces( aString );
+
+    // ignore leading white spaces
+    String::size_type aFieldStart( 0 );
+    String::size_type aFieldEnd( aString.
+				 find_first_of( DELIMITER,
+						aFieldStart ) );
+    
+     // absolute path ( start with '/' )
+    if( aFieldEnd == 0 )
+       {
+	 //	 push_back( String( 1, DELIMITER ) );
+	 push_back( "/" );
+	 ++aFieldStart;
+	 aFieldEnd = aString.find_first_of( DELIMITER, aFieldStart );
+
+	 if( aFieldEnd == String::npos )
+	   {
+	     return;
+	   }
+       }
+
+    while( 1 )
+      {
+	push_back( aString.substr( aFieldStart, 
+				   aFieldEnd - aFieldStart ) );
+	
+	if( aFieldEnd == String::npos )
+	  {
+	    return;
+	  }
+	aFieldStart = aFieldEnd + 1;
+	aFieldEnd = aString.find_first_of( DELIMITER, aFieldStart );
+      }
+
+  }
+
+  const String SystemPath::getString() const
+  {
+    StringListConstIterator i = begin();
+    String aString;
+
+    if( isAbsolute() )
+      {
+	if( size() == 1 )
+	  {
+	    return "/";
+	  }
+      }
+    else
+      {
+	// isAbsolute() == false implies that it can be empty
+	if( i == end() )
+	  {
+	    return "";
+	  }
+
+	aString = *i;
+      }
+
+
+    ++i;
+
+    while( i != end() )
+      {
+	aString += '/';
+	aString += *i;
+	++i;
+      }
+
+    return aString;
+  }
+
+
+
+  ///////////////// FullID
+
+  void FullID::parse( StringCref fullidstring )
+  {
+    // empty FullID string is invalid
+    if( fullidstring == "" )
+      {
+	throw BadID( __PRETTY_FUNCTION__,
+		     "Empty FullID string." );
+      }
+
+    String aString( fullidstring );
+    eraseWhiteSpaces( aString );
+
+    // ignore leading white spaces
+    String::size_type aFieldStart( 0 );
+    String::size_type aFieldEnd( aString.find_first_of( DELIMITER,
+							     aFieldStart ) );
+    if( aFieldEnd == String::npos )
+      {
+	throw BadID( __PRETTY_FUNCTION__,
+		     "No ':' in the FullID string [" + aString + "]." );
+      }
+
+    String aTypeString( aString.substr( aFieldStart, 
+					     aFieldEnd - aFieldStart ) );
+    thePrimitiveType = PrimitiveTypeOf( aTypeString );
+
+    aFieldStart = aFieldEnd + 1;
+    aFieldEnd = aString.find_first_of( DELIMITER, aFieldStart );
+    if( aFieldEnd == String::npos )
+      {
+	throw BadID( __PRETTY_FUNCTION__,
+		     "Only one ':' in the FullID string [" 
+		     + aString + "]." );
+      }
+
+    theSystemPath = 
+      SystemPath( aString.substr( aFieldStart, 
+				       aFieldEnd - aFieldStart ) );
+
+    aFieldStart = aFieldEnd + 1;
+
+    // drop trailing string after extra ':'(if this is  FullPropertyName),
+    // or go to the end
+    aFieldEnd = aString.find_first_of( DELIMITER, aFieldStart );
+
+    theID = aString.substr( aFieldStart, aFieldEnd - aFieldStart );
+  }    
+
+  const String FullID::getString() const
+  {
+    return PrimitiveTypeStringOf( thePrimitiveType ) + FullID::DELIMITER 
+      + theSystemPath.getString() + FullID::DELIMITER + theID;
+  }
+
+  bool FullID::isValid() const
+  {
+    bool aFlag( theSystemPath.isValid() );
+    aFlag &= ! theID.empty();
+
+    return aFlag;
+  }
+
+
+  ///////////////// FullPropertyName
+
+
+  FullPropertyName::FullPropertyName( StringCref fullpropertynamestring )
+    :
+    theFullID( fullpropertynamestring )
+  {
+
+    String::size_type aPosition( 0 );
+
+    for( Int i( 0 ) ; i < 3 ; ++i )
+      {
+	aPosition = fullpropertynamestring.
+	  find_first_of( FullID::DELIMITER, aPosition );
+	if( aPosition == String::npos ) 
+	  {
+	    throw BadID( __PRETTY_FUNCTION__, 
+			 "Not enough fields in FullPropertyName string [" +
+			 fullpropertynamestring + "]." );
+	  }
+	++aPosition;
+      }
+
+    thePropertyName = fullpropertynamestring.substr( aPosition, String::npos );
+    eraseWhiteSpaces( thePropertyName );
+  }
+
+  const String FullPropertyName::getString() const
+  {
+    return theFullID.getString() + FullID::DELIMITER + thePropertyName;
+  }
+
+  bool FullPropertyName::isValid() const
+  {
+    return theFullID.isValid() & ! thePropertyName.empty();
+  }
+
+} // namespace libecs
+
+#ifdef TEST_FQPI
+
+using namespace libecs;
+
+main()
+{
+  SystemPath aSystemPath( "   \t  /A/BB/CCC//DDDD/EEEEEE    \t \n  " );
+  cout << aSystemPath.getString() << endl;
+
+  SystemPath aSystemPath2( aSystemPath );
+  cout << aSystemPath2.getString() << endl;
+  
+  aSystemPath2.pop_front();
+  aSystemPath2.pop_back();
+  cout << aSystemPath2.getString() << endl;
+
+  SystemPath aSystemPath3( "/" );
+  cout << aSystemPath3.getString() << endl;
+  cout << aSystemPath3.size() << endl;
+  aSystemPath3.pop_front();
+  cout << aSystemPath3.getString() << endl;
+  cout << aSystemPath3.size() << endl;
+  cout << aSystemPath3.empty() << endl;
+
+  while( aSystemPath.size() != 0 )
+    {
+      cout << aSystemPath.size() << " : " << aSystemPath.isAbsolute() << " : " 
+	   << aSystemPath.getString() << endl;
+      aSystemPath.pop_front();
+    }
+
+  //  SystemPath aSystemPath2( "/A/../B" );
+  //  cout << aSystemPath2.getString() << endl;
+
+  cout << "\n::::::::::" << endl;
+
+  try
+    {
+      FullID aFullID( "       \t  \n  Substance:/A/B:S   \t   \n" );
+      cout << aFullID.getString() << endl;
+      cout << aFullID.getPrimitiveType() << endl;
+      cout << aFullID.getSystemPath().getString() << endl;
+      cout << aFullID.getID() << endl;
+      cout << aFullID.isValid() << endl;
+
+      FullID aFullID2( aFullID );
+      cout << aFullID2.getString() << endl;
+
+      FullID aFullID3( "Reactor:/:R" );
+      cout << aFullID3.getString() << endl;
+      aFullID3 = aFullID2;
+      cout << aFullID3.getString() << endl;
+
+      cout << "\n::::::::::" << endl;
+
+      FullPropertyName 
+	aFullPropertyName( "       \t  \n  Substance:/A/B:S:PNAME   \t   \n" );
+      cout << aFullPropertyName.getString() << endl;
+      cout << aFullPropertyName.getPrimitiveType() << endl;
+      cout << aFullPropertyName.getSystemPath().getString() << endl;
+      cout << aFullPropertyName.getID() << endl;
+      cout << aFullPropertyName.getPropertyName() << endl;
+      cout << aFullPropertyName.isValid() << endl;
+
+      FullPropertyName aFullPropertyName2( aFullPropertyName );
+      cout << aFullPropertyName2.getString() << endl;
+
+      FullPropertyName aFullPropertyName3( "Reactor:/:R:P" );
+      cout << aFullPropertyName3.getString() << endl;
+      aFullPropertyName3 = aFullPropertyName2;
+      cout << aFullPropertyName3.getString() << endl;
+
+    }
+  catch ( ExceptionCref e )
+    {
+      cerr << e.message() << endl;
+    }
+
+}
+
+
+#endif
+
+
+/*
+  Do not modify
+  $Author$
+  $Revision$
+  $Date$
+  $Locker$
+*/
---
+++ ecell/libecs/FullID.hpp
@@ -0,0 +1,360 @@
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//
+//        This file is part of E-CELL Simulation Environment package
+//
+//                Copyright (C) 1996-2000 Keio University
+//
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//
+//
+// E-CELL is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+// 
+// E-CELL is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public
+// License along with E-CELL -- see the file COPYING.
+// If not, write to the Free Software Foundation, Inc.,
+// 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+// 
+//END_HEADER
+//
+// written by Kouichi Takahashi <shafi@e-cell.org> at
+// E-CELL Project, Lab. for Bioinformatics, Keio University.
+//
+
+#ifndef ___FULLID_H___
+#define ___FULLID_H___
+#include <string>
+
+#include "libecs.hpp"
+#include "PrimitiveType.hpp"
+
+
+namespace libecs
+{
+
+  /** 
+      SystemPath 
+  */
+  class SystemPath : public StringList
+  {
+
+  public:
+
+    SystemPath( StringCref systempathstring = "" )
+    {
+      parse( systempathstring );
+    }
+
+    SystemPath( SystemPathCref systempath )
+      :
+      StringList( static_cast<StringList>( systempath ) )
+    {
+      ; // do nothing
+    }
+
+    ~SystemPath() {}
+
+    const String getString() const;
+    operator const String()  const  { return getString(); }
+
+    bool isAbsolute() const
+    {
+      return front()[0] == DELIMITER;
+    }
+
+    bool isValid() const
+    {
+      // FIXME: check '..'s and '.'s etc..
+      return true;
+    }
+
+  protected:
+
+    /**
+       Standardize a SystemPath. 
+       Reduce '..'s and remove extra white spaces.
+
+       @return reference to the systempath
+    */
+    void standardize();
+
+  private:
+
+    //    SystemPath();
+
+
+    void parse( StringCref systempathstring );
+
+  public:
+
+    static const char DELIMITER = '/';
+
+  };
+
+
+  /**
+     FullID is an identifier of a unique Entiy in a cell model.
+     The FullID consists of a PrimitiveType, a SystemPath and an ID string.
+
+     @see PrimitiveType, SystemPath
+  */
+  class FullID
+  {
+
+  public:
+
+    FullID( const PrimitiveType type,
+	    SystemPathCref systempath,
+	    StringCref id )
+      :
+      thePrimitiveType( type ),
+      theSystemPath( systempath ),
+      theID( id )
+    {
+      ; // do nothing
+    }
+
+    FullID( StringCref fullidstring )
+      :
+      thePrimitiveType( ENTITY ),
+      theSystemPath(),
+      theID()
+    {
+      parse( fullidstring );
+    }
+
+    FullID( FullIDCref fullid )
+      :
+      thePrimitiveType( fullid.getPrimitiveType() ),
+      theSystemPath( fullid.getSystemPath() ),
+      theID( fullid.getID() )
+    {
+      ; // do nothing
+    }
+
+
+    ~FullID() {}
+  
+    const PrimitiveType  getPrimitiveType() const 
+    { 
+      return thePrimitiveType; 
+    }
+
+    const SystemPathCref getSystemPath()    const 
+    { 
+      return theSystemPath; 
+    }
+
+    const StringCref     getID()            const 
+    { 
+      return theID;
+    }
+
+
+    void setPrimitiveType( const PrimitiveType type )
+    {
+      thePrimitiveType = type;
+    }
+
+    void setSystemPath( SystemPathCref systempath ) 
+    {
+      theSystemPath = systempath;
+    }
+
+    void setID( StringCref id ) 
+    {
+      theID = id;
+    }
+
+    bool isValid() const;
+
+    const String getString() const;
+    operator const String()  const  { return getString(); }
+
+    bool operator<( FullIDCref rhs ) const
+    {
+      // first look at the PrimitiveType
+      if( getPrimitiveType() != rhs.getPrimitiveType() )
+	{
+	  return getPrimitiveType() < rhs.getPrimitiveType();
+	}
+
+      // then compare the SystemPaths
+      // FIXME: should be faster is there is SystemPath::compare()
+      if( getSystemPath() != rhs.getSystemPath() )
+	{
+	  return getSystemPath() < rhs.getSystemPath();
+	}
+
+      // finally compare the ID strings
+      return getID() < rhs.getID();
+    }
+
+    bool operator==( FullIDCref rhs ) const
+    {
+      // first look at the PrimitiveType
+      if( getPrimitiveType() != rhs.getPrimitiveType() )
+	{
+	  return false;
+	}
+
+      // then compare the SystemPaths
+      if( getSystemPath() != rhs.getSystemPath() )
+	{
+	  return false;
+	}
+
+      // finally compare the ID strings
+      return getID() == rhs.getID();
+    }
+
+  protected:
+
+    void parse( StringCref fullidstring );
+
+  private:
+
+    FullID();
+
+  public:
+
+    static const char DELIMITER = ':';
+
+  private:
+
+    PrimitiveType thePrimitiveType;
+    SystemPath    theSystemPath;
+    String        theID;
+
+  };
+
+  class FullPropertyName
+  {
+
+  public:
+
+    FullPropertyName( const PrimitiveType type, 
+		      SystemPathCref systempath,
+		      StringCref id,
+		      StringCref propertyname )
+      :
+      theFullID( type, systempath, id ),
+      thePropertyName( propertyname )
+    {
+      ; // do nothing
+    }
+
+    FullPropertyName( FullIDCref fullid, StringCref propertyname )
+      :
+      theFullID( fullid ),
+      thePropertyName( propertyname )
+    {
+      ; // do nothing
+    }
+
+    //    FullPropertyName( FullPropertyNameCref fullpropertyname );
+
+    FullPropertyName( StringCref fullpropertynamestring );
+
+    ~FullPropertyName() {}
+
+
+
+    const FullIDCref     getFullID()        const
+    {
+      return theFullID;
+    }
+
+    const PrimitiveType  getPrimitiveType() const 
+    { 
+      return getFullID().getPrimitiveType(); 
+    }
+
+    const SystemPathCref getSystemPath()    const
+    { 
+      return getFullID().getSystemPath();
+    }
+
+    const StringCref     getID()            const
+    { 
+      return getFullID().getID();
+    }
+
+    const StringCref     getPropertyName()  const
+    {
+      return thePropertyName;
+    }
+
+    void setPrimitiveType( const PrimitiveType type )
+    {
+      theFullID.setPrimitiveType( type );
+    }
+
+    void setSystemPath( SystemPathCref systempath ) 
+    {
+      theFullID.setSystemPath( systempath );
+    }
+
+    void setID( StringCref id ) 
+    {
+      theFullID.setID( id );
+    }
+
+    void setPropertyName( StringCref propertyname )
+    {
+      thePropertyName = propertyname;
+    }
+
+    const String getString() const;
+    operator const String()  const  { return getString(); }
+
+    bool isValid() const;
+
+    bool operator<( FullPropertyNameCref rhs ) const
+    {
+      if( getFullID() != rhs.getFullID() )
+	{
+	  return getFullID() < rhs.getFullID();
+	}
+
+      return getPropertyName() < rhs.getPropertyName();
+    }
+
+    bool operator==( FullPropertyNameCref rhs ) const
+    {
+      if( getFullID() != rhs.getFullID() )
+	{
+	  return false;
+	}
+
+      // finally compare the ID strings
+      return getPropertyName() == rhs.getPropertyName();
+    }
+
+  private:
+
+    FullID theFullID;
+    String thePropertyName;
+
+  };
+
+
+
+} // namespace libecs
+
+#endif /*  ___FQPI_H___ */
+
+/*
+  Do not modify
+  $Author$
+  $Revision$
+  $Date$
+  $Locker$
+*/
