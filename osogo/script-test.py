b19075c4cd704e8c74a26996d9e96b3f0b4bbcf8
zhaiteng <zhaiteng@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2001-09-07 05:52:56 +0000

    MainWindow -> aMainWindow
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@365 f1531174-cb10-0410-9fe6-89aa7ac3eedb

--- osogo/script-test.py
+++ osogo/script-test.py
@@ -7,44 +7,44 @@ SUBSTANCE  = 2
 REACTOR    = 3
 SYSTEM     = 4
 
-MainWindow.theSimulator.createEntity( 'System', ( SYSTEM, '/', 'CELL' ), 'cell' )
-MainWindow.theSimulator.createEntity( 'System', ( SYSTEM, '/CELL', 'CYTOPLASM' ), 'cytoplasm' )
-MainWindow.theSimulator.createEntity( 'System', ( SYSTEM, '/CELL', 'MEMBRANE' ), 'membrane' )
-MainWindow.theSimulator.createEntity( 'System', ( SYSTEM, '/', 'ENVIRONMENT' ), 'environment' )
+aMainWindow.theSimulator.createEntity( 'System', ( SYSTEM, '/', 'CELL' ), 'cell' )
+aMainWindow.theSimulator.createEntity( 'System', ( SYSTEM, '/CELL', 'CYTOPLASM' ), 'cytoplasm' )
+aMainWindow.theSimulator.createEntity( 'System', ( SYSTEM, '/CELL', 'MEMBRANE' ), 'membrane' )
+aMainWindow.theSimulator.createEntity( 'System', ( SYSTEM, '/', 'ENVIRONMENT' ), 'environment' )
 
 print 'make substances...'
-MainWindow.theSimulator.createEntity( 'Substance', ( SUBSTANCE, '/', 'A' ), 'substance A' )
-MainWindow.theSimulator.createEntity( 'Substance', ( SUBSTANCE, '/', 'B' ), 'substance B' )
-MainWindow.theSimulator.createEntity( 'Substance', ( SUBSTANCE, '/', 'C' ), 'substance C' )
+aMainWindow.theSimulator.createEntity( 'Substance', ( SUBSTANCE, '/', 'A' ), 'substance A' )
+aMainWindow.theSimulator.createEntity( 'Substance', ( SUBSTANCE, '/', 'B' ), 'substance B' )
+aMainWindow.theSimulator.createEntity( 'Substance', ( SUBSTANCE, '/', 'C' ), 'substance C' )
 
-#  MainWindow.theSimulator.createEntity( 'Substance', ( SUBSTANCE, '/CELL/CYTOPLASM', 'ATP' ), 'substance ATP' )
-#  MainWindow.theSimulator.createEntity( 'Substance', ( SUBSTANCE, '/CELL/CYTOPLASM', 'ADP' ), 'substance ADP' )
-#  MainWindow.theSimulator.createEntity( 'Substance', ( SUBSTANCE, '/CELL/CYTOPLASM', 'AMP' ), 'substance AMP' )
+#  aMainWindow.theSimulator.createEntity( 'Substance', ( SUBSTANCE, '/CELL/CYTOPLASM', 'ATP' ), 'substance ATP' )
+#  aMainWindow.theSimulator.createEntity( 'Substance', ( SUBSTANCE, '/CELL/CYTOPLASM', 'ADP' ), 'substance ADP' )
+#  aMainWindow.theSimulator.createEntity( 'Substance', ( SUBSTANCE, '/CELL/CYTOPLASM', 'AMP' ), 'substance AMP' )
 
-#  MainWindow.theSimulator.createEntity( 'Substance', ( SUBSTANCE, '/ENVIRONMENT', 'GLU' ), 'Glucose' )
-#  MainWindow.theSimulator.createEntity( 'Substance', ( SUBSTANCE, '/ENVIRONMENT', 'PYR' ), 'Pyruvate' )
-#  MainWindow.theSimulator.createEntity( 'Substance', ( SUBSTANCE, '/ENVIRONMENT', 'LCT' ), 'Lactate' )
+#  aMainWindow.theSimulator.createEntity( 'Substance', ( SUBSTANCE, '/ENVIRONMENT', 'GLU' ), 'Glucose' )
+#  aMainWindow.theSimulator.createEntity( 'Substance', ( SUBSTANCE, '/ENVIRONMENT', 'PYR' ), 'Pyruvate' )
+#  aMainWindow.theSimulator.createEntity( 'Substance', ( SUBSTANCE, '/ENVIRONMENT', 'LCT' ), 'Lactate' )
 
-#  MainWindow.theSimulator.createEntity( 'Substance', ( SUBSTANCE, '/CELL/MEMBRANE', 'CI' ), 'Channel 1' )
-#  MainWindow.theSimulator.createEntity( 'Substance', ( SUBSTANCE, '/CELL/MEMBRANE', 'CII' ), 'Channel 2' )
-#  MainWindow.theSimulator.createEntity( 'Substance', ( SUBSTANCE, '/CELL/MEMBRANE', 'CIII' ), 'Channel 3' )
+#  aMainWindow.theSimulator.createEntity( 'Substance', ( SUBSTANCE, '/CELL/MEMBRANE', 'CI' ), 'Channel 1' )
+#  aMainWindow.theSimulator.createEntity( 'Substance', ( SUBSTANCE, '/CELL/MEMBRANE', 'CII' ), 'Channel 2' )
+#  aMainWindow.theSimulator.createEntity( 'Substance', ( SUBSTANCE, '/CELL/MEMBRANE', 'CIII' ), 'Channel 3' )
 
 
 print 'make reactors...'
 try:
-    MainWindow.theSimulator.createEntity('ConstantActivityReactor',
+    aMainWindow.theSimulator.createEntity('ConstantActivityReactor',
                                          ( REACTOR, '/CELL/CYTOPLASM', 'RC1' ),
                                          'constant reactor' )
 except:
     print 'cannot instantiate ConstantActivityReactor'
    
 #  print 'set Substance:/CELL/CYTOPLASM:ATP Quantity = 30'
-#  MainWindow.theSimulator.setProperty( ( SUBSTANCE, '/CELL/CYTOPLASM', 'ATP', 'Quantity' ), (30,) )
+#  aMainWindow.theSimulator.setProperty( ( SUBSTANCE, '/CELL/CYTOPLASM', 'ATP', 'Quantity' ), (30,) )
 
 print 'initialize()...'
-MainWindow.theSimulator.initialize()
+aMainWindow.theSimulator.initialize()
 
 
-printAllProperties( MainWindow.theSimulator, ( SYSTEM, '', '/' ) )
-printAllProperties( MainWindow.theSimulator, ( SYSTEM, '/', 'CYTOPLASM' ) )
-printProperty( MainWindow.theSimulator, ( SUBSTANCE, '/', 'A', 'Quantity' ) )
+printAllProperties( aMainWindow.theSimulator, ( SYSTEM, '', '/' ) )
+printAllProperties( aMainWindow.theSimulator, ( SYSTEM, '/', 'CYTOPLASM' ) )
+printProperty( aMainWindow.theSimulator, ( SUBSTANCE, '/', 'A', 'Quantity' ) )
