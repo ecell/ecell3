fe4ce9280541e3cf8f1de4f86b4a88e8a94e98d5
zhaiteng <zhaiteng@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2001-09-10 07:19:56 +0000

    *** empty log message ***
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@399 f1531174-cb10-0410-9fe6-89aa7ac3eedb

--- osogo/A-script-test.py
+++ osogo/A-script-test.py
@@ -2,24 +2,16 @@
 
 from ecssupport import *
 
-ENTITY     = 1
-SUBSTANCE  = 2
-REACTOR    = 3
-SYSTEM     = 4
-
 aSimulator = aMainWindow.theSimulator
 
-aMainWindow.printMessage("load rule\n")
+aMainWindow.printMessage("Load Rule...\n")
 
 aSimulator.createEntity( 'System', ( SYSTEM, '/', 'CELL' ), 'cell' )
 aSimulator.createEntity( 'System', ( SYSTEM, '/CELL', 'CYTOPLASM' ), 'cytoplasm' )
 aSimulator.createEntity( 'System', ( SYSTEM, '/CELL', 'MEMBRANE' ), 'membrane' )
 aSimulator.createEntity( 'System', ( SYSTEM, '/', 'ENVIRONMENT' ), 'environment' )
 
-aMainWindow.printMessage('make substances...')
-#  aSimulator.createEntity( 'Substance', ( SUBSTANCE, '/', 'A' ), 'substance A' )
-#  aSimulator.createEntity( 'Substance', ( SUBSTANCE, '/', 'B' ), 'substance B' )
-#  aSimulator.createEntity( 'Substance', ( SUBSTANCE, '/', 'C' ), 'substance C' )
+aMainWindow.printMessage('make Substances...')
 
 aSimulator.createEntity( 'Substance', ( SUBSTANCE, '/CELL/CYTOPLASM', 'ATP' ), 'substance ATP' )
 aSimulator.createEntity( 'Substance', ( SUBSTANCE, '/CELL/CYTOPLASM', 'ADP' ), 'substance ADP' )
@@ -34,24 +26,47 @@ aSimulator.createEntity( 'Substance', ( SUBSTANCE, '/CELL/MEMBRANE', 'CII' ), 'C
 aSimulator.createEntity( 'Substance', ( SUBSTANCE, '/CELL/MEMBRANE', 'CIII' ), 'Channel 3' )
 
 
-#  aMainWindow.printMessage("make reactors...\n")
-#  try:
-#      aSimulator.createEntity('ConstantActivityReactor',
-#                                           ( REACTOR, '/CELL/CYTOPLASM', 'RC1' ),
-#                                           'constant reactor' )
-#  except:
-#      print 'cannot instantiate ConstantActivityReactor'
-   
-aMainWindow.printMessage('set Substance:/CELL/CYTOPLASM:ATP Quantity = 30')
-aSimulator.setProperty( ( SUBSTANCE, '/CELL/CYTOPLASM', 'ATP', 'Quantity' ), (30,) )
+aMainWindow.printMessage("make Reactors...\n")
+try:
+    aSimulator.createEntity('ConstantActivityReactor',
+                                         ( REACTOR, '/CELL/CYTOPLASM', 'RC1' ),
+                                         'constant reactor' )
+except:
+   aMainWindow.printMessage("cannot instantiate ConstantActivityReactor\n")
+ 
+aMainWindow.printMessage("set Quantity...\n")
 
-#  aMainWindow.printMessage("initialize()...\n")
-#  aSimulator.initialize()
+aSimulator.setProperty( ( SUBSTANCE, '/CELL/CYTOPLASM', 'ATP', 'Quantity' ), (100,) )
+aSimulator.setProperty( ( SUBSTANCE, '/CELL/CYTOPLASM', 'ADP' , 'Quantity' ), (120,) )
+aSimulator.setProperty( ( SUBSTANCE, '/CELL/CYTOPLASM', 'AMP', 'Quantity' ), (140,) )
+
+aSimulator.setProperty( ( SUBSTANCE, '/ENVIRONMENT', 'GLU', 'Quantity' ), (300,) )
+aSimulator.setProperty( ( SUBSTANCE, '/ENVIRONMENT', 'PYR', 'Quantity' ), (400,) )
+aSimulator.setProperty( ( SUBSTANCE, '/ENVIRONMENT', 'LCT', 'Quantity' ), (500,) )
+
+aSimulator.setProperty( ( SUBSTANCE, '/CELL/MEMBRANE', 'CI', 'Quantity' ), (10,) )
+aSimulator.setProperty( ( SUBSTANCE, '/CELL/MEMBRANE', 'CII', 'Quantity' ), (20,) )
+aSimulator.setProperty( ( SUBSTANCE, '/CELL/MEMBRANE', 'CIII', 'Quantity' ), (30,) )
+
+aMainWindow.printMessage("initialize()...\n")
+aSimulator.initialize()
 
 #  aMainWindow.printAllProperties( ( SYSTEM, '', '/' ) )
 #  aMainWindow.printAllProperties( ( SYSTEM, '/', 'CYTOPLASM' ) )
+
 #  aMainWindow.printProperty( ( SUBSTANCE, '/', 'A', 'Quantity' ) )
 
+aMainWindow.printProperty( ( SUBSTANCE, '/CELL/CYTOPLASM', 'ATP', 'Quantity' ) )
+aMainWindow.printProperty( ( SUBSTANCE, '/CELL/CYTOPLASM', 'ADP', 'Quantity' ) )
+aMainWindow.printProperty( ( SUBSTANCE, '/CELL/CYTOPLASM', 'AMP', 'Quantity' ) )
+
+aMainWindow.printProperty( ( SUBSTANCE, '/ENVIRONMENT', 'GLU', 'Quantity' ) )
+aMainWindow.printProperty( ( SUBSTANCE, '/ENVIRONMENT', 'PYR', 'Quantity' ) )
+aMainWindow.printProperty( ( SUBSTANCE, '/ENVIRONMENT', 'LCT', 'Quantity' ) )
+
+aMainWindow.printProperty( ( SUBSTANCE, '/CELL/MEMBRANE', 'CI', 'Quantity' ) )
+aMainWindow.printProperty( ( SUBSTANCE, '/CELL/MEMBRANE', 'CII', 'Quantity' ) )
+aMainWindow.printProperty( ( SUBSTANCE, '/CELL/MEMBRANE', 'CIII', 'Quantity' ) )
 
 
 
