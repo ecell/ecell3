a5dca6a6c84d305e9a1beb816b1bd16dbcb3bd8a
kem <kem@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2001-09-12 02:53:54 +0000

    Property Name can be selected
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@439 f1531174-cb10-0410-9fe6-89aa7ac3eedb

--- osogo/EntryListWindow.py
+++ osogo/EntryListWindow.py
@@ -25,7 +25,6 @@ class EntryListWindow(Window):
                             'entry_list_selection_changed' : self.selectEntity
                             } )
 
-
         self.theMainWindow = aMainWindow
         self.theSimulator = aMainWindow.theSimulator
         self.thePaletteWindow = aMainWindow.thePaletteWindow
@@ -49,10 +48,16 @@ class EntryListWindow(Window):
         self.thePropertyWindow = aPManager.createInstance( 'PropertyWindow',
                                                            self.theSimulator,
                                                            () )
+#        aPropertyWindowTopVBox = self.thePropertyWindow['scrolledwindow1']
+#        aPropertyWindowVBox = self.thePropertyWindow['vbox2']
+#        aPropertyWindowVBox.unparent()
+#        self['property_frame'].add( aPropertyWindowVBox )
+
         aPropertyWindowTopVBox = self.thePropertyWindow['top_vbox']
         aPropertyWindowTopVBox.unparent()
         self['property_frame'].add( aPropertyWindowTopVBox )
         self.thePropertyWindow['window'].hide()
+        self.thePropertyWindow['property_clist'].connect( 'select_row', self.selectPropertyName )
         
         self.update()
 
@@ -112,11 +117,11 @@ class EntryListWindow(Window):
 
             aSystemPath = createSystemPathFromFullID( aSystemFullID )
 
-#            aEntityFullPN = ( aEntityType, aSystemPath, aEntityID, '' )
-            aEntityFullPN = ( aEntityType, aSystemPath, aEntityID, 'Quantity' )
+            aEntityFullPN = ( aEntityType, aSystemPath, aEntityID, '' )
+#            aEntityFullPN = ( aEntityType, aSystemPath, aEntityID, 'Quantity' )
 
             aListItem.set_data( 'FULLPN', aEntityFullPN)
-            aFullPNList = ( aEntityFullPN, )
+#            aFullPNList = ( aEntityFullPN, )
 
             self.theEntryList.add( aListItem )
             aListItem.show()
@@ -136,6 +141,16 @@ class EntryListWindow(Window):
 
         self.updateStatusBar()
 
+    def selectPropertyName( self, aCList, row, column, event_obj ):
+
+        self.theSelectedFullPNList = []
+        for aRowNumber in aCList.selection:
+            aPropertyName =  aCList.get_text( aRowNumber, 0 )
+            aFullID = self.thePropertyWindow.theFullID
+            aFullPN = convertFullIDToFullPN( aFullID, aPropertyName )
+            self.theSelectedFullPNList.append( aFullPN )
+        self.updateStatusBar()
+        
     def updateStatusBar( self ):
         aStatusString = 'Selected: '
         for aFullPN in self.theSelectedFullPNList:
