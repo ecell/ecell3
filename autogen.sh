68cc00201dc3ace6b6c9e5195e8fe044e9037bfa
shafi <shafi@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2001-11-03 17:48:37 +0000

    modifications for libtool-1.4
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@514 f1531174-cb10-0410-9fe6-89aa7ac3eedb

--- autogen.sh
+++ autogen.sh
@@ -10,25 +10,18 @@ PROJECT=E-CELL
 (autoconf --version) < /dev/null > /dev/null 2>&1 || {
         echo
         echo "You must have autoconf installed to compile $PROJECT."
-        echo "Download the appropriate package for your distribution,"
-        echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
         DIE=1
 }
 
-# Do we really need libtool?
-#(libtool --version) < /dev/null > /dev/null 2>&1 || {
-#        echo
-#        echo "You must have libtool installed to compile $PROJECT."
-#        echo "Get ftp://ftp.gnu.org/pub/gnu/libtool-1.2.tar.gz"
-#        echo "(or a newer version if it is available)"
-#        DIE=1
-#}
+(libtoolize --version) < /dev/null > /dev/null 2>&1 || {
+        echo
+        echo "You must have libtool installed to compile $PROJECT."
+        DIE=1
+}
 
 (automake --version) < /dev/null > /dev/null 2>&1 || {
         echo
         echo "You must have automake installed to compile $PROJECT."
-        echo "Get ftp://ftp.gnu.org/pub/gnu/automake-1.3.tar.gz"
-        echo "(or a newer version if it is available)"
         DIE=1
 }
 
@@ -39,6 +32,8 @@ fi
 if [ ! -f ChangeLog ] ; then
     touch ChangeLog
 fi
+
+#libtoolize --ltdl
 
 
 if test -z "$*"; then
