71c33b545c626d4c3d2169ebbc45d0f0b9940158
smash <smash@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2001-11-27 05:26:12 +0000

    rename from StlDataPointVector
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@537 f1531174-cb10-0410-9fe6-89aa7ac3eedb

---
+++ ecell/libecs/DataPointStlVector.hpp
@@ -0,0 +1,74 @@
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//
+//        This file is part of E-CELL Simulation Environment package
+//
+//                Copyright (C) 2000-2001 Keio University
+//
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//
+//
+// E-CELL is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+// 
+// E-CELL is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public
+// License along with E-CELL -- see the file COPYING.
+// If not, write to the Free Software Foundation, Inc.,
+// 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+// 
+//END_HEADER
+//
+// written by Masayuki Okayama <smash@e-cell.org> at
+// E-CELL Project, Lab. for Bioinformatics, Keio University.
+//
+
+
+#if !defined(__DATAPOINT_STL_VECTOR_HPP)
+#define __DATAPOINT_STL_VECTOR_HPP
+
+#include <vector>
+
+#include "libecs.hpp"
+
+#include "DataPoint.hpp"
+
+namespace libecs
+{
+
+  /**
+
+   */
+  DECLARE_CLASS(StlDataPointVector);
+    DECLARE_TYPE(DataPoint,Containee);
+    DECLARE_VECTOR(ContaineePtr,Container);
+
+  class DataPointStlVector
+    :
+    public Container
+  {
+
+  public:
+
+    ~DataPointStlVector(void);
+
+    void push( ContaineeCref );
+
+    void push( RealCref, UVariableCref );
+
+    const_iterator binary_search( const_iterator first,
+				  const_iterator last,
+				  RealCref) const;
+
+
+  };
+
+
+} // namespace libecs
+
+#endif /* __DATAPOINT_STL_VECTOR_HPP */
