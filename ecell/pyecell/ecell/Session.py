9c5eee507b3f7c51265644f154cd7b11ff031884
suzuki <suzuki@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2002-04-14 13:56:57 +0000

    loadModel has been added
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@639 f1531174-cb10-0410-9fe6-89aa7ac3eedb

--- ecell/pyecell/ecell/Session.py
+++ ecell/pyecell/ecell/Session.py
@@ -46,7 +46,12 @@ class Session:
         self.thePrintMethod( message )
 
     def loadModel( self, aModel ):
-        pass
+        self.__thePreModel = aPreModel
+
+        self.loadEntity()
+        self.loadStepper()
+        self.loadProperty()
+
         
     def saveModel( self ):
         pass
@@ -55,6 +60,88 @@ class Session:
         print aMessage
 
 
+
+
+
+
+    def loadStepper( self ):
+        """stepper loader"""
+        import string
+
+        for aTargetStepper in( self.__thePreModel[ 'stepper' ] ):
+            aClass = aTargetStepper[0]
+            anId   = aTargetStepper[1]
+
+            ## TemporarySample ---------------------------------------
+            aPrintClass = "Session.theSimulator.createStepper('" + aClass + "',"
+            aPrintId     = "'" + anId + "')"
+            print aPrintClass, aPrintId ## Temporary
+            ## -------------------------------------------------------
+
+
+        
+        for aTargetStepperSystem in( self.__thePreModel[ 'stepper_system' ] ):
+
+            aFullPath = aTargetStepperSystem[0]
+            aFullPathList = aFullPath.split( '/' )
+
+            aClass  = aTargetStepperSystem[1]
+            aValueList = [ aClass ]
+            aName = 'StepperId'
+
+
+            if aFullPath == '/':
+                aFullPn = 'SYSTEM::/:' + aName
+                
+            else:
+                anId    = aFullPathList[-1]
+                aFullPathList.remove( aFullPathList[ len(aFullPathList) - 1 ] )
+                aPath = string.join( aFullPathList, '/' )
+                if aPath == '':
+                    aPath = '/'
+                aFullPn = 'SYSTEM' + ':' + aPath + ':' + anId + ':' + aName
+            
+            ## TemporarySample ---------------------------------------
+            aPrintFullId = "Session.theSimulator.setProperty('" + aFullPn + "',"
+            print aPrintFullId, aValueList, ")"
+            ##-------------------------------------------------------
+
+
+
+    def loadEntity( self ):
+        """Entity loader"""
+        
+        for aTargetEntity in( self.__thePreModel[ 'entity' ] ):
+            aFullId = aTargetEntity[0]
+            aType   = aTargetEntity[1]
+            aName   = aTargetEntity[2]
+
+            # self.theDriver.createEntity( aType, aFullId, aName )
+
+
+            ## TemporarySample ---------------------------------------
+            aPrintFullId = "Session.theSimulator.createEntity('" + aFullId + "',"
+            aPrintType   = "'" + aType + "',"
+            aPrintName   = "'" + aName + "')"
+            print aPrintFullId, aPrintType, aPrintName ## Temporary
+            ## -------------------------------------------------------
+
+
+
+    def loadProperty( self ):
+        """Property loader"""
+        
+        for aTargetProperty in( self.__thePreModel[ 'property' ] ):
+            aFullPn = aTargetProperty[0]
+            aValue  = aTargetProperty[1]
+
+            #self.theDriver.setProperty( aFullPn, aValue )
+
+            
+            ## TemporarySample ---------------------------------
+            aPrintFullPn = "Session.theSimulator.setProperty('" + aFullPn + "',"
+            print aPrintFullPn, aValue, ')'
+            ## -------------------------------------------------
 
 
 
