698785e4fc4eb805ea23e982021cc23da37b92e9
shafi <shafi@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2002-04-15 12:56:34 +0000

    bugfix
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@651 f1531174-cb10-0410-9fe6-89aa7ac3eedb

--- ecell/pyecell/ecell/util.py
+++ ecell/pyecell/ecell/util.py
@@ -17,14 +17,3 @@ def printAllProperties( s, fullid ):
         except:
             print "failed to print %s:%s" % ( fullid, property )
 
-
-# deprecated, should not be used
-def isNumber( s, aFullPN ):
-
-    aValue = self.getProperty( s, aFullPN )
-    if type( aValue[0] ) is types.IntType:
-        return 1
-    elif type( aValue[0] ) is types.FloatType:
-        return 1
-    else:
-        return 0
