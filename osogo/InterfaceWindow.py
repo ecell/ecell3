dbc270ba13a05bc00e659c4d9b6dc8891de193a2
sugi <sugi@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2002-06-30 06:42:51 +0000

    The function of addNewRecord method was separated to update and addNewRecord.  - update method has the function that updates view of the list.  - addNewRecord method has the function that adds the list data.
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@760 f1531174-cb10-0410-9fe6-89aa7ac3eedb

--- osogo/InterfaceWindow.py
+++ osogo/InterfaceWindow.py
@@ -1,114 +1,134 @@
 #!/usr/bin/env python
 
-from Window import *
+#from Window import *
+from OsogoWindow import *
 from gtk import *
 from ecell.ecssupport import *
 from string import *
 
-class InterfaceWindow( Window ):
+#class InterfaceWindow( Window ):
+class InterfaceWindow( OsogoWindow ):
     
-    def __init__( self, aMainWindow ):
+	def __init__( self, aMainWindow ):
 
-        Window.__init__( self )
+		#Window.__init__( self )
+		OsogoWindow.__init__( self )
+		OsogoWindow.openWindow(self)
 
-	self.theMainWindow = aMainWindow
-	self.theInterfaceList = self[ 'InterfaceCList' ]
-        self.thePluginWindowsNoDict = {}
-	self.thePluginWindowsDict = {}
-        self.theCount = 0
-        self.theSelectedRow = -1
-        self.theTitle = ""
+		self.theMainWindow = aMainWindow
+		self.theInterfaceList = self[ 'InterfaceCList' ]
+		self.theInterfaceListBuffer = []
+		self.thePluginWindowsNoDict = {}
+		self.thePluginWindowsDict = {}
+		self.theCount = 0
+		self.theSelectedRow = -1
+		self.theTitle = ""
 
-        self.addHandlers( { 'interfacelist_select_row' : self.rowSelected,
-	   		    'ShowButton_clicked'       : self.showWindow,
-			    'SaveButton_clicked'       : self.editTitle,
-			    'DeleteButton_clicked'     : self.deleteWindow } )
+		# __init__
 
-    def addNewRecord( self , classname , data ):
+		self.addHandlers( { 'interfacelist_select_row' : self.rowSelected,
+		                    'ShowButton_clicked'       : self.showWindow,
+		                    'SaveButton_clicked'       : self.editTitle,
+		                    'DeleteButton_clicked'     : self.deleteWindow } )
+	
+
+	def update( self ):
+	
+		self.theInterfaceList.clear()
+		self[ 'InterfaceCList' ].clear()
+
+		for aList in self.theInterfaceListBuffer:
+			#self.theInterfaceList.append( aList )
+			self[ 'InterfaceCList' ].append( aList )
+
+	def addNewRecord( self , classname , data ):
         
-        aClassName = classname[ : -6 ]
-	aFullPN = createFullPNString( data[0] )
-	self.theTitle = self.createNewTitle( aClassName )
-	self.thePluginWindowsNoDict[ aClassName ] += 1
+		if len(data) != 0:
+
+			aClassName = classname[ : -6 ]
+			aFullPN = createFullPNString( data[0] )
+			self.theTitle = self.createNewTitle( aClassName )
+			self.thePluginWindowsNoDict[ aClassName ] += 1
         
-	alist = ( self.theTitle , aClassName , aFullPN )
-	self.theInterfaceList.append( alist )
-	self.thePluginWindowsDict[ self.theTitle ] = self.theCount
-	self.theCount += 1
+			alist = ( self.theTitle , aClassName , aFullPN )
+			#self.theInterfaceList.append( alist )
+			self.theInterfaceListBuffer.append( alist )
+			self.thePluginWindowsDict[ self.theTitle ] = self.theCount
+			self.theCount += 1
 
-    def createNewTitle( self , aClassName ):
+		self.update()
 
-	aTitle = aClassName + str( self.thePluginWindowsNoDict[ aClassName ] + 1 )
-	return aTitle
+	def createNewTitle( self , aClassName ):
 
-    def removeRecord( self , obj ):
+		aTitle = aClassName + str( self.thePluginWindowsNoDict[ aClassName ] + 1 )
+		return aTitle
+
+	def removeRecord( self , obj ):
 		
-	aWindowName = obj.getWidget( obj.__class__.__name__ )[ 'title' ]
-	aRowNum = self.thePluginWindowsDict[ aWindowName ]
-		
-	for aKey in self.thePluginWindowsDict.keys():
-	    if self.thePluginWindowsDict[ aKey ] > aRowNum:
-	        self.thePluginWindowsDict[ aKey ] -= 1
-	    elif self.thePluginWindowsDict[ aKey ] == aRowNum:
-		del self.thePluginWindowsDict[ aKey ]
+		aWindowName = obj.getWidget( obj.__class__.__name__ )[ 'title' ]
 
-	self.theInterfaceList.remove( aRowNum )
-	self.theCount -= 1
-	self[ "SelectedTitle" ].set_text( "" )
+		aRowNum = self.thePluginWindowsDict[ aWindowName ]
 
-    def editTitle( self , obj ):
+		for aKey in self.thePluginWindowsDict.keys():
+			if self.thePluginWindowsDict[ aKey ] > aRowNum:
+				self.thePluginWindowsDict[ aKey ] -= 1
+			elif self.thePluginWindowsDict[ aKey ] == aRowNum:
+				del self.thePluginWindowsDict[ aKey ]
+
+		self.theInterfaceList.remove( aRowNum )
+		self.theCount -= 1
+		self[ "SelectedTitle" ].set_text( "" )
+
+	def editTitle( self , obj ):
         
-        try:
-            if self[ "SelectedTitle" ].get_text() !=  "" and self[ "SelectedTitle" ].get_text() != self.theInterfaceList.get_text( self.theSelectedRow , 0 ):
-                for aKey in self.thePluginWindowsDict.keys():
-                    if aKey == self[ "SelectedTitle" ].get_text():
-                        aSameNameFlag = 1    #aSameNameFlag = 1 if title already exists 
-                        break
-                    else:
-                        aSameNameFlag = 0
+		try:
+			if self[ "SelectedTitle" ].get_text() !=  "" and self[ "SelectedTitle" ].get_text() != self.theInterfaceList.get_text( self.theSelectedRow , 0 ):
+				for aKey in self.thePluginWindowsDict.keys():
+					if aKey == self[ "SelectedTitle" ].get_text():
+						aSameNameFlag = 1    #aSameNameFlag = 1 if title already exists 
+						break
+					else:
+						aSameNameFlag = 0
 
-                if aSameNameFlag == 0:
-                    for aKey in self.thePluginWindowsDict.keys():
-                        if self.thePluginWindowsDict[ aKey ] == self.theSelectedRow:
-                            del self.thePluginWindowsDict[ aKey ]
+				if aSameNameFlag == 0:
+					for aKey in self.thePluginWindowsDict.keys():
+						if self.thePluginWindowsDict[ aKey ] == self.theSelectedRow:
+							del self.thePluginWindowsDict[ aKey ]
 			
-                    self.thePluginWindowsDict[ self[ "SelectedTitle" ].get_text() ] = self.theSelectedRow
-                    self.theInterfaceList.set_text( self.theSelectedRow , 0 , self[ "SelectedTitle" ].get_text() )
-                    self.theMainWindow.thePluginManager.getModule( self.theSelectedRow , self[ "SelectedTitle" ].get_text())
+					self.thePluginWindowsDict[ self[ "SelectedTitle" ].get_text() ] = self.theSelectedRow
+					self.theInterfaceList.set_text( self.theSelectedRow , 0 , self[ "SelectedTitle" ].get_text() )
+					self.theMainWindow.thePluginManager.getModule( self.theSelectedRow , self[ "SelectedTitle" ].get_text())
 
-                else:
-                    self[ "SelectedTitle" ].set_text( self.theInterfaceList.get_text( self.theSelectedRow, 0 ) )
+				else:
+					self[ "SelectedTitle" ].set_text( self.theInterfaceList.get_text( self.theSelectedRow, 0 ) )
 
-        except ValueError:
-            pass
+		except ValueError:
+			pass
 			
-    def rowSelected( self , obj , row , column , data3 ):
+	def rowSelected( self , obj , row , column , data3 ):
 
-	self.theSelectedRow = row
-	self[ "SelectedTitle" ].set_text( self.theInterfaceList.get_text( self.theSelectedRow , 0 ))
+		self.theSelectedRow = row
+		self[ "SelectedTitle" ].set_text( self.theInterfaceList.get_text( self.theSelectedRow , 0 ))
 
-    def showWindow( self , obj ):
+	def showWindow( self , obj ):
 
-        if self.theSelectedRow != -1:
-            self.theMainWindow.thePluginManager.showPlugin( self.theSelectedRow , obj )
+		if self.theSelectedRow != -1:
+			self.theMainWindow.thePluginManager.showPlugin( self.theSelectedRow , obj )
                     
-    def deleteWindow( self , obj ):
+	def deleteWindow( self , obj ):
 
-        if self.theSelectedRow != -1:
-            self.theMainWindow.thePluginManager.deleteModule( self.theSelectedRow , obj )
+		if self.theSelectedRow != -1:
+			self.theMainWindow.thePluginManager.deleteModule( self.theSelectedRow , obj )
 
 if __name__ == "__main__":
 
-    def mainLoop():
+	def mainLoop():
+		gtk.mainloop()
 
-        gtk.mainloop()
+	def main():
+			aWindow = InterfaceWindow( 'InterfaceWindow.glade' )
+			mainLoop()
 
-    def main():
-
-        aWindow = InterfaceWindow( 'InterfaceWindow.glade' )
-        mainLoop()
-
-
-    main()
+	main()
 
 
