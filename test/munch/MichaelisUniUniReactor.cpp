19290a51c1e213d799292f6eef49efaa78071395
tomo <tomo@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2002-07-04 12:32:20 +0000

    for munch and munch files
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@787 f1531174-cb10-0410-9fe6-89aa7ac3eedb

---
+++ test/munch/MichaelisUniUniReactor.cpp
@@ -0,0 +1,121 @@
+#line 1 "/home/tomo/wrk/munch/dmtemplates/reactor/reactor.tmpl"
+
+
+#include "libecs/libecs.hpp"
+#include "FluxReactor.hpp"
+
+namespace libecs
+{
+
+  class MichaelisUniUniReactor
+    :  
+    public FluxReactor
+  {
+  
+  public:
+
+    MichaelisUniUniReactor();
+    ~MichaelisUniUniReactor();
+
+    void setKmS( RealCref value ) { KmS = value; }
+    const Real getKmS() const { return KmS; }
+    void setKcF( RealCref value ) { KcF = value; }
+    const Real getKcF() const { return KcF; }
+
+
+
+
+    virtual void initialize();
+
+    static ReactorPtr createInstance() 
+    { 
+      return new MichaelisUniUniReactor;
+    }
+   
+    StringLiteral getClassName() const { return "MichaelisUniUniReactor"; }
+
+
+  protected:
+    
+    void makeSlots();
+            
+    Real KmS;
+    Real KcF;
+
+
+PropertySlotPtr S0_Concentration;
+PropertySlotPtr C0_Quantity;
+
+
+  };
+
+
+}
+
+
+#include <iostream>
+
+#include "libecs/System.hpp"
+#include "libecs/Substance.hpp"
+#include "libecs/Util.hpp"
+
+
+//#include "MichaelisUniUniReactor.hpp"
+
+
+using namespace libecs;
+
+extern "C"
+{
+  ReactorAllocatorFunc CreateObject =
+  &MichaelisUniUniReactor::createInstance;
+}  
+
+MichaelisUniUniReactor::MichaelisUniUniReactor()
+{
+  makeSlots();
+    KmS = 0.0;
+    KcF = 0.0;
+
+}
+
+MichaelisUniUniReactor::~MichaelisUniUniReactor()
+{
+}
+
+void MichaelisUniUniReactor::makeSlots()
+{
+createPropertySlot( "KmS", *this, &MichaelisUniUniReactor::setKmS, &MichaelisUniUniReactor::getKmS );
+createPropertySlot( "KcF", *this, &MichaelisUniUniReactor::setKcF, &MichaelisUniUniReactor::getKcF );
+
+}
+
+
+void MichaelisUniUniReactor::initialize()
+{
+  FluxReactor::initialize();
+
+S0_Concentration = getPropertySlotOfReactant( "S0", "Concentration" );
+C0_Quantity = getPropertySlotOfReactant( "C0", "Quantity" );
+
+
+
+}
+
+
+void MichaelisUniUniReactor::differentiate()
+{
+#line 83 "/home/tomo/wrk/munch/dmtemplates/reactor/reactor.tmpl"
+
+  Real velocity( KcF );
+  velocity *= C0_Quantity->getReal();
+  const Real S( S0_Concentration->getReal() );
+  velocity *= S;
+  velocity /= ( KmS + S );
+
+  process( velocity );
+
+}
+
+#line 91 "/home/tomo/wrk/munch/dmtemplates/reactor/reactor.tmpl"
+
