209eae262a4f689806186158a589e3ff9a166abd
tkitayama <tkitayama@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2003-02-26 08:50:14 +0000

    rewrite
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@1273 f1531174-cb10-0410-9fe6-89aa7ac3eedb

--- ecell/libecs/dmtemplates/DM.py
+++ ecell/libecs/dmtemplates/DM.py
@@ -1,50 +1,22 @@
 import string
 
-def ECELL_DM_BEGIN( classname, type ):
-    filename = identify()[0]
-    print "#include \"libecs.hpp\""
-    print "#include \"System.hpp\""
-    print "#include \"Stepper.hpp\""
-    print "#include \"Variable.hpp\""
-    print "#include \"VariableProxy.hpp\""
-    print "#include \"FluxProcess.hpp\""
-    print "#include \"Process.hpp\""
-    print "#include \"Util.hpp\""
-    print "#include \"PropertyInterface.hpp\""
-    print "#include \"PropertySlotMaker.hpp\"\n"
-    
-    print "#define SIMPLE_SET_GET_METHOD( TYPE, NAME )\\"
-    print "SIMPLE_SET_METHOD( TYPE, NAME )\\"
-    print "SIMPLE_GET_METHOD( TYPE, NAME )\n"
-    print "#define SIMPLE_GET_METHOD( TYPE, NAME )\\"
-    print "const TYPE get ## NAME() const\\"
-    print "{\\"
-    print "return NAME;\\"
-    print "}\n"
-    
-    print "#define SIMPLE_SET_METHOD( TYPE, NAME )\\"
-    print "void set ## NAME( TYPE ## Cref value )\\"
-    print "{\\"
-    print "NAME = value;\\"
-    print "}\n"
-
-    print "#define XSTR( S ) STR( S )"
-    print "#define STR( S ) #S"
-    print "#define ECELL_OBJECT \\"
-    print "StringLiteral getClassname() { return XSTR( _ECELL_CLASSNAME ); }\\"
-    print "static _ECELL_TYPE* createInstance() { return new _ECELL_CLASSNAME ; }\n"
-    print "#define _ECELL_TYPE %s" % type
-    print "#define _ECELL_CLASSNAME %s" % classname
-
-    print 'namespace libecs { \n'
-    print "#line 1 \"%s\"" % filename
+def ECELL_DM_BEGIN( type, classname ):
+    filename, line = identify()
+    print '''
+    #define __ECELL_TYPE %s
+    #define __ECELL_CLASSNAME %s
+    #define LIBECS_DM_OBJECT( __ECELL_TYPE, __ECELL_CLASSNAME )
+    namespace libecs { \n
+    #line %s \"%s\"''' % ( type, classname, line, filename )
     
 def ECELL_DM_END():
-    print "extern \"C\""
-    print "{"
-    print "  _ECELL_TYPE::AllocatorFuncPtr CreateObject ="
-    print "  &_ECELL_CLASSNAME::createInstance;"
-    print "}"
-    print '}'
-
-    print '#undef _ECELL_CLASSNAME\n#undef _ECELL_TYPE'
+    print '''
+    extern \"C\"
+    {
+      _ECELL_TYPE::AllocatorFuncPtr CreateObject =
+        &_ECELL_CLASSNAME::createInstance;
+      }
+    }
+    #undef __ECELL_CLASSNAME
+    #undef __ECELL_TYPE
+    '''
