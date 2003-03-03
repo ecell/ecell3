faf802cbe1c39fc560452d8bfd76fc04352a8429
tkitayama <tkitayama@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2003-03-03 04:55:31 +0000

    for new dm & bug fix
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@1349 f1531174-cb10-0410-9fe6-89aa7ac3eedb

--- doc/sample/Drosophila/FM1Process.cpp
+++ doc/sample/Drosophila/FM1Process.cpp
@@ -12,6 +12,7 @@
 #include "ecell3_dm.hpp"
 
 #define ECELL3_DM_TYPE Process
+#define ECELL3_DM_CLASSNAME FM1Process
 
 USE_LIBECS;
 
@@ -19,42 +20,49 @@ ECELL3_DM_CLASS
   :  
   public FluxProcess
 {
-  
+
   ECELL3_DM_OBJECT;
-  
+
  public:
 
    ECELL3_DM_CLASSNAME()
-    {
-      ECELL3_CREATE_PROPERTYSLOT_SET_GET( Real, vs );
-      ECELL3_CREATE_PROPERTYSLOT_SET_GET( Real, KI );
-    }
+     {
+       ECELL3_CREATE_PROPERTYSLOT_SET_GET( Real, vs );
+       ECELL3_CREATE_PROPERTYSLOT_SET_GET( Real, KI );
+     }
 
-  SIMPLE_SET_GET_METHOD( Real, vs );
-  SIMPLE_SET_GET_METHOD( Real, KI );
+   SIMPLE_SET_GET_METHOD( Real, vs );
+   SIMPLE_SET_GET_METHOD( Real, KI );
+   //void setvs( RealCref value ) { vs = value; }
+   //const Real getvs() const { return vs; }
+   //void setKI( RealCref value ) { KI = value; }
+   //const Real getKI() const { return KI; }
 
-  virtual void initialize()
-    {
-      FluxProcess::initialize();
-      C0 = getVariableReference( "C0" );
-    }
+    virtual void initialize()
+      {
+	FluxProcess::initialize();
+	C0 = getVariableReference( "C0" );
+      }
 
-  virtual void process()
-    {
-      Real E( C0.getConcentration() );
-      Real V( vs * KI );
-      V /= KI + (E * E * E);
-      V *= 1E-018 * N_A;
-      setFlux( V );
-    }
+    virtual void process()
+      {
+	Real E( C0.getConcentration() );
+	Real V( vs * KI );
+	V /= KI + (E * E * E);
+	V *= 1E-018 * N_A;
+	setFlux( V );
+      }
 
  protected:
 
-  Real vs;
-  Real KI;
+    Real vs;
+    Real KI;
 
-  VariableReference C0;
+    VariableReference C0;
+
+  private:
 
 };
 
 ECELL3_DM_INIT;
+
