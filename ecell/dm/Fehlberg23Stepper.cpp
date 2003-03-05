d3673b4bd624244b5992c60c9e6386404d6fcf35
kaizu <kaizu@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2003-03-05 07:56:09 +0000

    *** empty log message ***
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@1396 f1531174-cb10-0410-9fe6-89aa7ac3eedb

---
+++ ecell/dm/Fehlberg23Stepper.cpp
@@ -0,0 +1,170 @@
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//
+//        This file is part of E-CELL Simulation Environment package
+//
+//                Copyright (C) 2002 Keio University
+//
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//
+//
+// E-CELL is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+// 
+// E-CELL is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public
+// License along with E-CELL -- see the file COPYING.
+// If not, write to the Free Software Foundation, Inc.,
+// 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+// 
+//END_HEADER
+//
+// written by Kouichi Takahashi <shafi@e-cell.org> at
+// E-CELL Project, Lab. for Bioinformatics, Keio University.
+//
+
+#include "Variable.hpp"
+
+#include "Fehlberg23Stepper.hpp"
+
+DM_INIT( Stepper, Fehlberg23Stepper );
+
+namespace libecs
+{
+
+  Fehlberg23Stepper::Fehlberg23Stepper()
+  {
+    ; // do nothing
+  }
+	    
+  Fehlberg23Stepper::~Fehlberg23Stepper()
+  {
+    ; // do nothing
+  }
+
+  void Fehlberg23Stepper::initialize()
+  {
+    AdaptiveDifferentialStepper::initialize();
+
+    // the number of write variables
+    const UnsignedInt aSize( getReadOnlyVariableOffset() );
+
+    theK1.resize( aSize );
+  }
+
+  bool Fehlberg23Stepper::calculate()
+  {
+    const UnsignedInt aSize( getReadOnlyVariableOffset() );
+
+    const Real eps_rel( getTolerance() );
+    const Real eps_abs( getTolerance() * getAbsoluteToleranceFactor() );
+    const Real a_y( getStateToleranceFactor() );
+    const Real a_dydt( getDerivativeToleranceFactor() );
+
+    const Real aCurrentTime( getCurrentTime() );
+
+    // ========= 1 ===========
+    process();
+
+    for( UnsignedInt c( 0 ); c < aSize; ++c )
+      {
+	VariablePtr const aVariable( theVariableVector[ c ] );
+	    
+	// get k1
+	const Real aVelocity( aVariable->getVelocity() );
+	theK1[ c ] = aVelocity;
+	
+	// restore k1
+	aVariable->loadValue( aVelocity * getStepInterval() 
+			      + theValueBuffer[ c ] );
+
+	// clear velocity
+	aVariable->setVelocity( 0.0 );
+      }
+
+    // ========= 2 ===========
+    setCurrentTime( aCurrentTime + getStepInterval() );
+    process();
+
+    for( UnsignedInt c( 0 ); c < aSize; ++c )
+      {
+	VariablePtr const aVariable( theVariableVector[ c ] );
+	    
+	// get k2
+	const Real aVelocity( aVariable->getVelocity() );
+	theVelocityBuffer[ c ] = aVelocity;
+	    
+	aVariable->loadValue( ( aVelocity + theK1[ c ] ) * 0.25 
+			      * getStepInterval() 
+			      + theValueBuffer[ c ] );
+	    
+	// clear velocity
+	aVariable->setVelocity( 0.0 );
+      }
+	
+    // ========= 3 ===========
+    setCurrentTime( aCurrentTime + getStepInterval()*0.5 );
+    process();
+	
+    Real maxError( 0.0 );
+
+    // restore theValueBuffer
+    for( UnsignedInt c( 0 ); c < aSize; ++c )
+      {
+	VariablePtr const aVariable( theVariableVector[ c ] );
+	
+	const Real aVelocity( aVariable->getVelocity() );
+	
+	// ( k1 + k2 + k3 * 4 ) / 6 for ~Yn+1
+	const Real anErrorEstimate( ( theK1[ c ] + theVelocityBuffer[ c ]
+				      + aVelocity * 4.0 ) * ( 1.0 / 6.0 ) );
+
+	// ( k1 + k2 ) / 2 for Yn+1
+	theVelocityBuffer[ c ] += theK1[ c ];
+	theVelocityBuffer[ c ] *= 0.5;
+
+	const Real aTolerance( eps_rel *
+				( a_y * fabs( theValueBuffer[ c ] ) 
+				  +  a_dydt * fabs( theVelocityBuffer[ c ] ) )
+				+ eps_abs );
+
+	const Real anError( fabs( ( theVelocityBuffer[ c ] 
+				    - anErrorEstimate ) / aTolerance ) );
+
+	if( anError > maxError )
+	  {
+	    maxError = anError;
+	  }
+
+	// restore x (original value)
+	aVariable->loadValue( theValueBuffer[ c ] );
+
+	//// x(n+1) = x(n) + k2 * aStepInterval + O(h^3)
+	aVariable->setVelocity( theVelocityBuffer[ c ] );
+      }
+    
+    setMaxErrorRatio( maxError );
+
+    // reset the stepper current time
+    setCurrentTime( aCurrentTime );
+
+    if ( maxError > 1.1 )
+      {
+	reset();
+	setOriginalStepInterval( 0.0 );
+
+	return false;
+      }
+
+    // set the error limit interval
+    setOriginalStepInterval( getStepInterval() );
+
+    return true;
+  }
+
+}
