c3951d4799f0dc671eb5d6556d57284504f09819
tkitayama <tkitayama@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2003-03-09 06:36:29 +0000

    port DecayFluxReactor.rd from ecell1 standard-reactor
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@1424 f1531174-cb10-0410-9fe6-89aa7ac3eedb

---
+++ ecell/dm/DecayFluxProcess.cpp
@@ -0,0 +1,69 @@
+// Warning:
+//
+// the number of Substrate of DecayReactor must be one.
+//
+
+#include "libecs.hpp"
+#include "Process.hpp"
+#include "Util.hpp"
+#include "PropertyInterface.hpp"
+#include "PropertySlotMaker.hpp"
+#include "System.hpp"
+#include "Stepper.hpp"
+#include "Variable.hpp"
+#include "VariableProxy.hpp"
+
+#include "FluxProcess.hpp"
+#include "ecell3_dm.hpp"
+
+#define ECELL3_DM_TYPE Process
+
+USE_LIBECS;
+
+ECELL3_DM_CLASS
+  :  
+  public FluxProcess
+{
+
+  ECELL3_DM_OBJECT;
+  
+ public:
+
+  ECELL3_DM_CLASSNAME()
+    {
+      ECELL3_CREATE_PROPERTYSLOT_SET_GET( Real, T );
+    }
+  
+  SIMPLE_SET_GET_METHOD( Real, T );
+    
+  virtual void initialize()
+    {
+      FluxProcess::initialize();
+
+      if(T <= 0)
+	{
+	  THROW_EXCEPTION( ValueError, "Error:in DecayFluxProcess::initialze().0 or negative half time. set to 1." );
+	}
+      
+      k = log(2)/T;
+      S0 = getVariableReference( "S0" );
+    }
+
+  virtual void process()
+    {
+      Real velocity( k * N_A );
+      velocity *= getSuperSystem()->getVolume();
+
+      velocity *= pow(S0.getVariable()->getConcentration(),S0.getCoefficient()*-1);
+      setFlux( velocity );
+    }
+
+ protected:
+  
+  Real T;
+  Real k;
+  VariableReference S0;
+  
+};
+
+ECELL3_DM_INIT;
