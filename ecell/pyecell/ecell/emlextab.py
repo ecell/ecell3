f3be5a4dfebe04a82f30dcd28d9fde88876dd865
taka-ken <taka-ken@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2003-03-10 09:02:29 +0000

    rename lextab.py for emparser
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@1452 f1531174-cb10-0410-9fe6-89aa7ac3eedb

---
+++ ecell/pyecell/ecell/emlextab.py
@@ -0,0 +1,32 @@
+# emlextab.py.  This file automatically created by PLY. Don't edit.
+_lexre = '(?P<t_Stepper> Stepper[\\s|\\t] )|(?P<t_System> System[\\s|\\t] )|(?P<t_Process> Process[\\s|\\t] )|(?P<t_Variable> Variable[\\s|\\t] )|(?P<t_LPAREN>\\()|(?P<t_RPAREN>\\))|(?P<t_LBRACKET>\\[)|(?P<t_RBRACKET>\\])|(?P<t_LBRACE>\\{)|(?P<t_RBRACE>\\})|(?P<t_SEMI>;)|(?P<t_number> [+-]?(\\d+(\\.\\d*)?|\\d*\\.\\d+)([eE][+-]?\\d+)? )|(?P<t_name>[a-zA-Z_/][\\w\\:\\/.]*)|(?P<t_quotedstring> "(^"|.)*" | \\\'(^\\\'|.)*\\\' )|(?P<t_control> \\%line [^\\n]*\\n )|(?P<t_comment> \\# [^\\n]* )|(?P<t_nl> \\n )|(?P<t_whitespace> [ |\\t]+ )|(?P<t_default> .+ )'
+_lextab = [
+  None,
+  ('t_Stepper','Stepper'),
+  ('t_System','System'),
+  ('t_Process','Process'),
+  ('t_Variable','Variable'),
+  ('t_LPAREN','LPAREN'),
+  ('t_RPAREN','RPAREN'),
+  ('t_LBRACKET','LBRACKET'),
+  ('t_RBRACKET','RBRACKET'),
+  ('t_LBRACE','LBRACE'),
+  ('t_RBRACE','RBRACE'),
+  ('t_SEMI','SEMI'),
+  ('t_number','number'),
+  None,
+  None,
+  None,
+  ('t_name','name'),
+  ('t_quotedstring','quotedstring'),
+  None,
+  None,
+  ('t_control','control'),
+  ('t_comment','comment'),
+  ('t_nl','nl'),
+  ('t_whitespace','whitespace'),
+  ('t_default','default'),
+]
+_lextokens = {'control': None, 'comment': None, 'LBRACE': None, 'nl': None, 'name': None, 'SEMI': None, 'Process': None, 'RPAREN': None, 'Stepper': None, 'System': None, 'RBRACE': None, 'quotedstring': None, 'LBRACKET': None, 'LPAREN': None, 'Variable': None, 'number': None, 'RBRACKET': None, 'whitespace': None}
+_lexignore = None
+_lexerrorf = 't_error'
