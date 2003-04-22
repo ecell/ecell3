053a994c6ca08a2700e0aba7d3c6832dc352e318
kmaru <kmaru@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2003-04-22 09:59:02 +0000

    change:gsl_vector->vector
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@1562 f1531174-cb10-0410-9fe6-89aa7ac3eedb

--- ecell/dm/SSystemProcess.cpp
+++ ecell/dm/SSystemProcess.cpp
@@ -1,6 +1,5 @@
-#include <gsl/gsl_vector.h>
-#include <gsl/gsl_matrix.h>
 #include <gsl/gsl_sf.h>
+#include <vector>
 
 #include "libecs.hpp"
 #include "System.hpp"
@@ -16,6 +15,8 @@
 
 #define ECELL3_DM_TYPE Process
 
+using namespace std;
+
 USE_LIBECS;
 
 ECELL3_DM_CLASS
@@ -28,37 +29,12 @@ ECELL3_DM_CLASS
  public:
 
   ECELL3_DM_CLASSNAME()
-    :
-    theSystemSize( 0 ),
-    Order( 3 ),
-    theChangedFlag( false ),
-    theY( NULLPTR ),
-    theAlpha( NULLPTR ),
-    theBeta( NULLPTR ),
-    theG( NULLPTR ),
-    theH( NULLPTR ),
-    theAlphaBuffer( NULLPTR ),
-    theBetaBuffer( NULLPTR ),
-    theGBuffer( NULLPTR ),
-    theHBuffer( NULLPTR ),
-    theFBuffer( NULLPTR )
     {
+      theSystemSize = 0;
+      Order = 3;
+
       ECELL3_CREATE_PROPERTYSLOT_SET_GET( Int, Order );
       ECELL3_CREATE_PROPERTYSLOT_SET_GET( Polymorph, SSystemMatrix );
-    }
-  
-  ~ECELL3_DM_CLASSNAME()
-    {
-      gsl_vector_free( theAlpha );
-      gsl_vector_free( theBeta );
-      gsl_matrix_free( theY );
-      gsl_matrix_free( theG );
-      gsl_matrix_free( theH );
-      gsl_matrix_free( theAlphaBuffer );
-      gsl_matrix_free( theBetaBuffer );
-      gsl_matrix_free( theGBuffer );
-      gsl_matrix_free( theHBuffer );
-      gsl_matrix_free( theFBuffer );
     }
 
   SIMPLE_GET_METHOD( Int, Order );
@@ -81,25 +57,25 @@ ECELL3_DM_CLASS
 
   Int Order;
   Int theSystemSize;
-  mutable bool theChangedFlag;
 
   Polymorph SSystemMatrix;
   
   // State variables in log space
-  gsl_matrix* theY;
+  vector< RealVector > theY;
   
   // S-System vectors
-  gsl_vector* theAlpha;
-  gsl_vector* theBeta;
-  gsl_matrix* theG;
-  gsl_matrix* theH;
+  RealVector theAlpha;
+  RealVector theBeta;
+
+  vector< RealVector > theG;
+  vector< RealVector > theH;
   
   // tmp S-System vectors
-  gsl_matrix* theAlphaBuffer;
-  gsl_matrix* theBetaBuffer;
-  gsl_matrix* theGBuffer;
-  gsl_matrix* theHBuffer;
-  gsl_matrix* theFBuffer;
+  vector< RealVector > theAlphaBuffer;
+  vector< RealVector > theBetaBuffer;
+  vector< RealVector > theGBuffer;
+  vector< RealVector > theHBuffer;
+  vector< RealVector > theFBuffer;
   
 };
 
@@ -110,39 +86,48 @@ using namespace libecs;
 void ECELL3_DM_CLASSNAME::setOrder( IntCref aValue ) 
 { 
   Order = aValue;
-
-  if( theChangedFlag )
+  
+  // init Substance Vector
+  theY.resize(theSystemSize+1);
+  RealVector tmp;
+  tmp.resize(Order+1);
+  for(Int i( 0 ); i < theSystemSize + 1; i++)
     {
-      // free Matrix & Vactor
-      gsl_vector_free( theAlpha );
-      gsl_vector_free( theBeta );
-      gsl_matrix_free( theY );
-      gsl_matrix_free( theG );
-      gsl_matrix_free( theH );
-      gsl_matrix_free( theAlphaBuffer );
-      gsl_matrix_free( theBetaBuffer );
-      gsl_matrix_free( theGBuffer );
-      gsl_matrix_free( theHBuffer );
-      gsl_matrix_free( theFBuffer );
+      theY[i] = tmp;
     }
 
-  // init Substance Vector
-  theY = gsl_matrix_calloc (theSystemSize+1,Order+1);
-
   // init S-System Vector & Matrix
-  theAlpha = gsl_vector_calloc (theSystemSize+1);
-  theBeta  = gsl_vector_calloc (theSystemSize+1);
-  theG = gsl_matrix_calloc (theSystemSize+1, theSystemSize+1);
-  theH = gsl_matrix_calloc (theSystemSize+1, theSystemSize+1);
+  theAlpha.resize(theSystemSize+1);
+  theBeta.resize(theSystemSize+1);
+  theG.resize( theSystemSize + 1);
+  theH.resize( theSystemSize + 1);
+  tmp.resize(theSystemSize+1);
+  for(Int i( 0 ); i < theSystemSize + 1; i++)
+    {
+      theG[i] = tmp;
+      theH[i] = tmp;
+    }
 
   // init S-System tmp Vector & Matrix
-  theAlphaBuffer = gsl_matrix_calloc (theSystemSize+1, Order+1);
-  theBetaBuffer  = gsl_matrix_calloc (theSystemSize+1, Order+1);
-  theGBuffer     = gsl_matrix_calloc (theSystemSize+1, Order+1);
-  theHBuffer     = gsl_matrix_calloc (theSystemSize+1, Order+1);
-  theFBuffer     = gsl_matrix_calloc (Order+1,Order);
+  theAlphaBuffer.resize( theSystemSize + 1);
+  theBetaBuffer.resize( theSystemSize + 1);
+  theGBuffer.resize( theSystemSize + 1);
+  theHBuffer.resize( theSystemSize + 1);
+  tmp.resize(Order+1);
+  for(Int i( 0 ); i < theSystemSize + 1; i++)
+    {
+      theAlphaBuffer[i] = tmp;
+      theBetaBuffer[i] = tmp;
+      theGBuffer[i] = tmp;
+      theHBuffer[i] = tmp;
+    }
 
-  theChangedFlag = true;
+  theFBuffer.resize( Order + 1);
+  tmp.resize(Order);
+  for(Int i( 0 ); i < Order + 1; i++)
+    {
+      theFBuffer[i] = tmp;
+    }  
 
 }
 
@@ -152,70 +137,82 @@ void ECELL3_DM_CLASSNAME::setSSystemMatrix( PolymorphCref aValue )
   PolymorphVector aValueVector( aValue.asPolymorphVector() );
   theSystemSize = aValueVector.size();
 
-  if( theChangedFlag )
+  // init Substance Vector
+  theY.resize(theSystemSize+1);
+  RealVector tmp;
+  tmp.resize(Order+1);
+  for(Int i( 0 ); i < theSystemSize + 1; i++)
     {
-      // free Matrix & Vactor
-      gsl_vector_free( theAlpha );
-      gsl_vector_free( theBeta );
-      gsl_matrix_free( theY );
-      gsl_matrix_free( theG );
-      gsl_matrix_free( theH );
-      gsl_matrix_free( theAlphaBuffer );
-      gsl_matrix_free( theBetaBuffer );
-      gsl_matrix_free( theGBuffer );
-      gsl_matrix_free( theHBuffer );
-      gsl_matrix_free( theFBuffer );
+      theY[i] = tmp;
     }
 
-  // init Substance Vector
-  theY = gsl_matrix_calloc (theSystemSize+1,Order+1);
-
   // init S-System Vector & Matrix
-  theAlpha = gsl_vector_calloc (theSystemSize+1);
-  theBeta  = gsl_vector_calloc (theSystemSize+1);
-  theG = gsl_matrix_calloc (theSystemSize+1, theSystemSize+1);
-  theH = gsl_matrix_calloc (theSystemSize+1, theSystemSize+1);
+  theAlpha.resize(theSystemSize+1);
+  theBeta.resize(theSystemSize+1);
+  theG.resize( theSystemSize + 1);
+  theH.resize( theSystemSize + 1);
+  tmp.resize(theSystemSize+1);
+  for(Int i( 0 ); i < theSystemSize + 1; i++)
+    {
+      theG[i] = tmp;
+      theH[i] = tmp;
+    }
 
   // init S-System tmp Vector & Matrix
-  theAlphaBuffer = gsl_matrix_calloc (theSystemSize+1, Order+1);
-  theBetaBuffer  = gsl_matrix_calloc (theSystemSize+1, Order+1);
-  theGBuffer     = gsl_matrix_calloc (theSystemSize+1, Order+1);
-  theHBuffer     = gsl_matrix_calloc (theSystemSize+1, Order+1);
-  theFBuffer     = gsl_matrix_calloc (Order+1,Order);
+  theAlphaBuffer.resize( theSystemSize + 1);
+  theBetaBuffer.resize( theSystemSize + 1);
+  theGBuffer.resize( theSystemSize + 1);
+  theHBuffer.resize( theSystemSize + 1);
+  tmp.resize(Order+1);
 
-  theChangedFlag = true;
+  for(Int i( 0 ); i < theSystemSize + 1; i++)
+    {
+      theAlphaBuffer[i] = tmp;
+      theBetaBuffer[i] = tmp;
+      theGBuffer[i] = tmp;
+      theHBuffer[i] = tmp;
+    }
+
+  theFBuffer.resize( Order + 1);
+  tmp.resize(Order);
+  for(Int i( 0 ); i < Order + 1; i++)
+    {
+      theFBuffer[i] = tmp;
+    }  
 
   // init Factorial matrix
-  for(int m( 2 ) ; m < Order+1 ; m++)
-  {
-    for(int q( 1 ); q < m ; q++)
+  for(Int m( 2 ) ; m < Order+1 ; m++)
     {
-      const Real aFact( 1 / gsl_sf_fact(q-1) * gsl_sf_fact(m-q-1) * m * (m-1) );
-      gsl_matrix_set( theFBuffer,m ,q ,aFact );
-     }
-  }
+      for(Int q( 1 ); q < m ; q++)
+	{
+	  const Real aFact( 1 / gsl_sf_fact(q-1) * gsl_sf_fact(m-q-1) * m * (m-1) );
+	  (theFBuffer[m])[q] = aFact;      
+	}
+    }
 
   // set Alpha, Beta, G, H 
   for( Int i( 0 ); i < theSystemSize; i++ )
     {
-      gsl_vector_set( theAlpha, i+1, (aValueVector[i].asPolymorphVector())[0].asReal() );
+      
+      theAlpha[i+1] = (aValueVector[i].asPolymorphVector())[0].asReal() ;
       for( Int j( 0 ); j < theSystemSize; j++ )	
 	{
 	  if( i == j )
 	    {
-	      gsl_matrix_set( theG, i+1, j+1, (aValueVector[i].asPolymorphVector())[j+1].asReal() - 1 );
+	      
+	      (theG[i+1])[j+1] = (aValueVector[i].asPolymorphVector())[j+1].asReal() - 1 ;
 	    }else{
-	      gsl_matrix_set( theG, i+1, j+1, (aValueVector[i].asPolymorphVector())[j+1].asReal() );
+	      (theG[i+1])[j+1] = (aValueVector[i].asPolymorphVector())[j+1].asReal() ;
 	    }
 	}
-      gsl_vector_set( theBeta, i+1, (aValueVector[i].asPolymorphVector())[1+theSystemSize].asReal() );
+      theBeta[i+1] = (aValueVector[i].asPolymorphVector())[1+theSystemSize].asReal() ;
       for( Int j( 0 ); j < theSystemSize; j++ )	
 	{
 	  if( i == j )
 	    {
-	      gsl_matrix_set( theH, i+1, j+1, (aValueVector[i].asPolymorphVector())[2+j+theSystemSize].asReal() -1 );
+	      (theH[i+1])[j+1] = (aValueVector[i].asPolymorphVector())[2+j+theSystemSize].asReal() -1 ;
 	    }else{
-	      gsl_matrix_set( theH, i+1, j+1, (aValueVector[i].asPolymorphVector())[2+j+theSystemSize].asReal() );
+	      (theH[i+1])[j+1] = (aValueVector[i].asPolymorphVector())[2+j+theSystemSize].asReal() ;
 	    }
 	}
     }
@@ -223,110 +220,110 @@ void ECELL3_DM_CLASSNAME::setSSystemMatrix( PolymorphCref aValue )
 
 void ECELL3_DM_CLASSNAME::process()
 {
-  //get theY
-  Int anIndex( 0 );
-  for( VariableReferenceVectorConstIterator
-	 i ( thePositiveVariableReferenceIterator );
-       i != theVariableReferenceVector.end() ; ++i )
-    {
-      if( (*i).getValue() <= 0 )
-	{
-	  THROW_EXCEPTION( ValueError, "Error:in SSystemPProcess::process().log() in 0.");
-	}
-
-      gsl_matrix_set(theY, anIndex, 0, 
-		     gsl_sf_log( (*i).getValue() ) );
-      anIndex++;
+ //get theY
+ Int anIndex = 0;
+ for( VariableReferenceVectorConstIterator
+	i ( thePositiveVariableReferenceIterator );
+      i != theVariableReferenceVector.end() ; ++i )
+   {
+     if( (*i).getVariable()->getValue() <= 0 )
+       {
+	 THROW_EXCEPTION( ValueError, "Error:in SSystemPProcess::process().log() in 0.");
+       }
+     (theY[anIndex])[0] =
+       gsl_sf_log( (*i).getVariable()->getValue() ) ;
+     anIndex++;
     }
 
-  //differentiate first order
-  for(int i( 1 ) ; i < theSystemSize+1 ; i++ )
+ //differentiate first order
+ for(Int i( 1 ) ; i < theSystemSize+1 ; i++ )
     {
-      double aGt( 0.0 );
-      double aHt( 0.0 );
-      for(int j( 1 ) ; j < theSystemSize+1 ; j++ )
+      Real aGt( 0.0 );
+      Real aHt( 0.0 );
+      for(Int j( 1 ) ; j < theSystemSize+1 ; j++ )
         {
-          aGt += gsl_matrix_get(theG, i, j) * gsl_matrix_get(theY, j-1, 0);
-          aHt += gsl_matrix_get(theH, i, j) * gsl_matrix_get(theY, j-1, 0);
+          aGt += (theG[i])[j] * (theY[j-1])[0];
+          aHt += (theH[i])[j] * (theY[j-1])[0];
         }
+      
+      Real aAlpha = theAlpha[i] * exp(aGt);
+      Real aBate  = theBeta[i] * exp(aHt);
 
-      double aAlpha = gsl_vector_get(theAlpha,i) * exp(aGt);
-      double aBate  = gsl_vector_get(theBeta,i) * exp(aHt);
-      gsl_matrix_set(theAlphaBuffer, i, 1, aAlpha);
-      gsl_matrix_set(theBetaBuffer, i, 1, aBate);
-      gsl_matrix_set(theY, i-1, 1, aAlpha - aBate);
+      (theAlphaBuffer[i])[1] = aAlpha;
+      (theBetaBuffer[i])[1] = aBate;
+      (theY[i-1])[1] =  aAlpha - aBate;
     }
-
-  //differentiate second and/or more order
-  for( int m( 2 ) ; m <= Order ; m++)
-    {
-      for(int i( 1 ) ; i < theSystemSize+1 ; i++ )
-	{
+ 
+ //differentiate second and/or more order
+ for( Int m( 2 ) ; m <= Order ; m++)
+   {
+     for(Int i( 1 ) ; i < theSystemSize+1 ; i++ )
+       {
+	 
+	 (theGBuffer[i])[m-1] = 0;
+	 (theHBuffer[i])[m-1] = 0;
 	  
-	  gsl_matrix_set(theGBuffer, i, m-1, 0);
-	  gsl_matrix_set(theHBuffer, i, m-1, 0);
-	  
-	  for(int j( 1 ) ; j < theSystemSize+1 ; j++ )
-	    {
-	      const Real aY( gsl_matrix_get(theY, j-1, m-1) );
-	      const Real aG( gsl_matrix_get(theGBuffer, i, m-1) );
-	      const Real aH( gsl_matrix_get(theHBuffer, i, m-1) );
+	 for(Int j( 1 ) ; j < theSystemSize+1 ; j++ )
+	   {
+	      const Real aY( (theY[j-1])[m-1] );
+	      const Real aG( (theGBuffer[i])[m-1] );
+	      const Real aH( (theHBuffer[i])[m-1] );
 	      
-	      gsl_matrix_set(theGBuffer, i, m-1, 
-			     aG + gsl_matrix_get(theG, i, j) * aY );
-	      gsl_matrix_set(theHBuffer, i, m-1, 
-			     aH + gsl_matrix_get(theH, i, j) * aY );
-	    }
+	      (theGBuffer[i])[m-1] = 
+		             aG + (theG[i])[j] * aY ;
+	      (theHBuffer[i])[m-1] =
+			     aH + (theH[i])[j] * aY ;
+	   }
 	}
       
-      for(int i( 1 ) ; i < theSystemSize+1 ; i++ )
+      for(Int i( 1 ) ; i < theSystemSize+1 ; i++ )
 	{
-	  gsl_matrix_set(theAlphaBuffer, i, m, 0);
-	  gsl_matrix_set(theBetaBuffer, i, m, 0);
+	  (theAlphaBuffer[i])[m] = 0;
+	  (theBetaBuffer[i])[m] = 0;
 	  
-	  for(int q( 1 );  0 > m-q ; q++)
+	  for(Int q( 1 );  0 > m-q ; q++)
 	    {
-	      gsl_matrix_set(theAlphaBuffer, i, m, 
-			     gsl_matrix_get(theAlphaBuffer, i, m) + 
-			     gsl_matrix_get(theFBuffer, m, q) *
-			     gsl_matrix_get(theAlphaBuffer, i, m-q) *
-			     gsl_matrix_get(theGBuffer, i, m-q));
-	      gsl_matrix_set(theBetaBuffer, i, m, 
-			     gsl_matrix_get(theBetaBuffer, i, m) + 
-			     gsl_matrix_get(theFBuffer, m, q) *
-			     gsl_matrix_get(theBetaBuffer, i, m-q) *
-			     gsl_matrix_get(theHBuffer, i, m-q));
+	      (theAlphaBuffer[i])[m] = 
+			     (theAlphaBuffer[i])[m] + 
+			     (theFBuffer[m])[q] *
+			     (theAlphaBuffer[i])[m-q] *
+			     (theGBuffer[i])[m-q] ;
+	      (theBetaBuffer[i])[m] =
+			     (theBetaBuffer[i])[m]  + 
+			     (theFBuffer[m])[q] *
+			     (theBetaBuffer[i])[m-q] *
+			     (theHBuffer[i])[m-q] ;
 	    }
 	  
-	  gsl_matrix_set(theY, i-1, m, 
-			 gsl_matrix_get(theAlphaBuffer, i, m) - 
-			 gsl_matrix_get(theBetaBuffer, i, m));
+	  (theY[i-1])[m] = 
+                       (theAlphaBuffer[i])[m] - 
+                       (theBetaBuffer[i])[m] ;
 	}
       
     }
 
-  const Real aStepInterval( getSuperSystem()->getStepper()->getStepInterval() );
-  
-  //integrate
-  for( Int i( 1 ); i < theSystemSize+1; i++)
-    {
-
-      Real aY( 0.0 );
+ const Real aStepInterval( getSuperSystem()->getStepper()->getStepInterval() );
+ 
+ //integrate
+ for( Int i( 1 ); i < theSystemSize+1; i++)
+   {
+     
+     Real aY( 0.0 );
       for( Int m( 1 ); m <= Order ; m++)
         {
-	    aY += gsl_matrix_get(theY, i-1, m) * 
-	      gsl_sf_pow_int(aStepInterval,m) / gsl_sf_fact(m);
+	  aY += (theY[i-1])[m] * 
+	    gsl_sf_pow_int(aStepInterval,m) / gsl_sf_fact(m);
 	}
-      gsl_matrix_set(theY, i-1, 0, aY + gsl_matrix_get(theY, i-1, 0));
+      (theY[i-1])[0] =  aY + (theY[i-1])[0] ;
     }
-  
-  //set value
+
+ //set value
   anIndex = 0;
   for( VariableReferenceVectorConstIterator
 	 i ( thePositiveVariableReferenceIterator );
        i != theVariableReferenceVector.end() ; ++i )
     {
-      (*i).setValue( exp( gsl_matrix_get(theY, anIndex, 0) ) );
+      (*i).getVariable()->setValue( exp( (theY[anIndex])[0] ) );
       anIndex++;
     }
 }
