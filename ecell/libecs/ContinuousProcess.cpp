6f6778993068b81321adb42715fa755bbf2345f5
kaizu <kaizu@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2003-08-25 02:23:33 +0000

    temporary update of ContinuousProcess
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@1765 f1531174-cb10-0410-9fe6-89aa7ac3eedb

---
+++ ecell/libecs/ContinuousProcess.cpp
@@ -0,0 +1,7 @@
+#include "libecs.hpp"
+#include "ContinuousProcess.hpp"
+
+namespace libecs
+{
+  LIBECS_DM_INIT_STATIC( ContinuousProcess, Process );
+}
---
+++ ecell/libecs/ContinuousProcess.hpp
@@ -0,0 +1,36 @@
+#include "libecs.hpp"
+#include "Process.hpp"
+
+namespace libecs
+{
+
+  LIBECS_DM_CLASS( ContinuousProcess, Process )
+  {
+
+  public:
+
+    LIBECS_DM_OBJECT_ABSTRACT( ContinuousProcess )
+      {
+	INHERIT_PROPERTIES( Process );
+      }
+  
+    ContinuousProcess()
+      {
+	; // do nothing
+      }
+  
+    virtual ~ContinuousProcess()
+      {
+	;
+      }
+
+    virtual const bool isContinuous() const
+    {
+      return true;
+    }
+
+  protected:
+  
+  };
+
+}
