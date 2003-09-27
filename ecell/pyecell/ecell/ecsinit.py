913e5467e34f37b5968b9fcce904475ff406c12b
satyanandavel <satyanandavel@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2003-09-27 06:22:46 +0000

    changes for compatibility in Windows environment
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@1832 f1531174-cb10-0410-9fe6-89aa7ac3eedb

--- ecell/pyecell/ecell/ecsinit.py
+++ ecell/pyecell/ecell/ecsinit.py
@@ -1,8 +1,12 @@
-import sys
-import DLFCN
+import os
 
-# RTLD_GLOBAL is needed so that rtti across dynamic modules can work
-# RTLD_LAZY   may be needed so that the system can resolve dependency among
-#             dynamic modules after dlopened it
-sys.setdlopenflags( DLFCN.RTLD_LAZY | DLFCN.RTLD_GLOBAL )
+if os.name != "nt":
+    import sys
+    import DLFCN
+    
+    # RTLD_GLOBAL is needed so that rtti across dynamic modules can work
+    # RTLD_LAZY   may be needed so that the system can resolve dependency among
+    #             dynamic modules after dlopened it
+    
+    sys.setdlopenflags( DLFCN.RTLD_LAZY | DLFCN.RTLD_GLOBAL )
 
