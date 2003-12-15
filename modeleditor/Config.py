1cf1fce9be24c371bb1781979367835c4b7812dc
satyanandavel <satyanandavel@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2003-12-15 04:19:31 +0000

    changes to support conversion of shell scripts to python scripts
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@1897 f1531174-cb10-0410-9fe6-89aa7ac3eedb

--- modeleditor/Config.py
+++ modeleditor/Config.py
@@ -1,10 +1,10 @@
-EDITOR_PATH = '/usr/local/src/usr/lib/modeleditor'
+EDITOR_PATH = '/home/satya/root/kona/lib/modeleditor'
 
 # where to place .glade files for main (non-plugin) windows
 GLADEFILE_PATH = EDITOR_PATH + '/glade'
 
-DM_PATH = '/usr/local/src/usr/lib/ecell/3.1.99'
+DM_PATH = '/home/satya/root/kona/lib/ecell/3.1.99'
 PACKAGE = 'modeleditor'
 VERSION = '0.1'
-GNOME_INSTALLED = 'yes'
+GNOME_INSTALLED = 'no'
 
