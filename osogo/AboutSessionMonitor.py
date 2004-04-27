0fe45423bf25ad7e8a5b21129c8e226f921df460
satyanandavel <satyanandavel@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2004-04-27 22:35:32 +0000

    Added new About dialog for Session Monitor
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@2052 f1531174-cb10-0410-9fe6-89aa7ac3eedb

---
+++ osogo/AboutSessionMonitor.py
@@ -0,0 +1,70 @@
+
+import gtk
+import gtk.glade
+import os
+import os.path
+import gtk.gdk
+
+from MainWindow import *
+
+
+class  AboutSessionMonitor:
+	
+	#######################
+	#    GENERAL CASES    #
+	#######################
+
+	def __init__( self, aMainWindow ):
+		"""
+		sets up a modal dialogwindow displaying 
+		the AboutSessionMonitor window
+             
+		""" 
+		self.theMainWindow = aMainWindow	
+		
+		filename = os.environ["OSOGOPATH"] + os.sep + "AboutSessionMonitor.glade"
+		widgets=gtk.glade.XML(filename,"attachment_box")
+		att_box = widgets.get_widget("attachment_box")
+#		widgets.get_widget("window1").remove( att_box )
+#		widgets.get_widget("window1").destroy()
+		# Create the Dialog
+		self.win = gtk.Dialog('AboutSessionMonitor' , None)
+		self.win.connect("destroy",self.destroy)
+
+		# Sets size and position
+		self.win.set_border_width(2)
+		self.win.set_default_size(300,75)
+		self.win.set_position(gtk.WIN_POS_MOUSE)
+
+		# appends ok button
+		ok_button = gtk.Button("  OK  ")
+		self.win.action_area.pack_start(ok_button,gtk.FALSE,gtk.FALSE,)
+		ok_button.set_flags(gtk.CAN_DEFAULT)
+		ok_button.grab_default()
+		ok_button.show()
+		ok_button.connect("clicked",self.destroy)
+		self.win.vbox.pack_start( att_box )
+		# Sets title
+		self.win.set_title('About Session Monitor')
+		iconPixbuf = None
+		try:
+			iconPixbuf = gtk.gdk.pixbuf_new_from_file(os.environ['OSOGOPATH'] + os.sep + "icon_ecelllogo.png")
+		except:
+			pass
+		self.win.set_icon(iconPixbuf)
+		widgets.get_widget("label1").set_markup("<b>E-Cell Session Monitor Version " + os.environ["VERSION"] + "</b>")
+		self.win.show_all()
+		
+		self.theMainWindow.toggleAboutSessionMonitor(True,self)
+		
+
+		
+	
+	def destroy( self, *arg ):
+		"""destroy dialog
+		"""
+		
+		self.win.destroy()
+
+		self.theMainWindow.toggleAboutSessionMonitor(False,None)
+		
