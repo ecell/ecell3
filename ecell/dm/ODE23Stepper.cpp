5c232759f080c4f327f7fe81f77c3f1dfab75658
kaizu <kaizu@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2004-06-10 01:27:21 +0000

    the prototype for efficient interIntegrate()
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@2132 f1531174-cb10-0410-9fe6-89aa7ac3eedb

--- ecell/dm/ODE23Stepper.cpp
+++ ecell/dm/ODE23Stepper.cpp
@@ -55,6 +55,25 @@ namespace libecs
     const VariableVector::size_type aSize( getReadOnlyVariableOffset() );
 
     theK1.resize( aSize );
+    theK2.resize( aSize );
+
+    // theVelocityBuffer can be replaced by theK2
+    // ODE23Stepper doesn't need it, but ODE45Stepper does for the efficiency 
+    theVelocityBuffer.clear();
+  }
+
+  void ODE23Stepper::interIntegrate2()
+  {
+    Real const aCurrentTime( getCurrentTime() );
+
+    for( VariableVector::size_type c( 0 );
+	 c != theVariableVector.size(); ++c )
+      {
+        VariablePtr const aVariable( theVariableVector[ c ] );
+
+        aVariable->loadValue( theValueBuffer[ c ] );
+        aVariable->interIntegrate( aCurrentTime );
+      }
   }
 
   bool ODE23Stepper::calculate()
@@ -69,6 +88,7 @@ namespace libecs
     const Real aCurrentTime( getCurrentTime() );
 
     // ========= 1 ===========
+    interIntegrate2();
     fireProcesses();
 
     for( VariableVector::size_type c( 0 ); c < aSize; ++c )
@@ -79,16 +99,13 @@ namespace libecs
 	const Real aVelocity( aVariable->getVelocity() );
 	theK1[ c ] = aVelocity;
 	
-	// restore k1
-	aVariable->loadValue( aVelocity * getStepInterval() 
-			      + theValueBuffer[ c ] );
-
 	// clear velocity
 	aVariable->clearVelocity();
       }
 
     // ========= 2 ===========
     setCurrentTime( aCurrentTime + getStepInterval() );
+    interIntegrate2();
     fireProcesses();
 
     for( VariableVector::size_type c( 0 ); c < aSize; ++c )
@@ -97,18 +114,15 @@ namespace libecs
 	    
 	// get k2
 	const Real aVelocity( aVariable->getVelocity() );
-	theVelocityBuffer[ c ] = aVelocity;
-	    
-	aVariable->loadValue( ( aVelocity + theK1[ c ] ) * 0.25 
-			      * getStepInterval() 
-			      + theValueBuffer[ c ] );
-	    
+	theK2[ c ] = aVelocity - theK1[ c ];
+    
 	// clear velocity
 	aVariable->clearVelocity();
       }
 	
     // ========= 3 ===========
-    setCurrentTime( aCurrentTime + getStepInterval()*0.5 );
+    setCurrentTime( aCurrentTime + getStepInterval() * 0.5 );
+    interIntegrate2();
     fireProcesses();
 	
     Real maxError( 0.0 );
@@ -119,22 +133,23 @@ namespace libecs
 	VariablePtr const aVariable( theVariableVector[ c ] );
 	
 	const Real aVelocity( aVariable->getVelocity() );
-	
-	// ( k1 + k2 + k3 * 4 ) / 6 for ~Yn+1
-	const Real anErrorEstimate( ( theK1[ c ] + theVelocityBuffer[ c ]
-				      + aVelocity * 4.0 ) * ( 1.0 / 6.0 ) );
+ 
+	// ( k1 - k2 ) / 2 for K2
+	theK2[ c ] *= 0.5;
 
-	// ( k1 + k2 ) / 2 for Yn+1
-	theVelocityBuffer[ c ] += theK1[ c ];
-	theVelocityBuffer[ c ] *= 0.5;
+	const Real anExpectedVelocity( theK1[ c ] + theK2[ c ] );
+
+	// ( k1 + k2 + k3 * 4 ) / 6 for ~Yn+1
+	// ( k1 + k2 - k3 * 2 ) / 3 for ( Yn+1 - ~Yn+1 ) as a local error
+	const Real anEstimatedError
+	  ( fabs( ( anExpectedVelocity - aVelocity ) * ( 2.0 / 3.0 ) ) );
 
 	const Real aTolerance( eps_rel *
 				( a_y * fabs( theValueBuffer[ c ] ) 
-				  +  a_dydt * fabs( theVelocityBuffer[ c ] ) )
+				  +  a_dydt * fabs( anExpectedVelocity ) )
 				+ eps_abs );
 
-	const Real anError( fabs( ( theVelocityBuffer[ c ] 
-				    - anErrorEstimate ) / aTolerance ) );
+	const Real anError( anEstimatedError / aTolerance );
 
 	if( anError > maxError )
 	  {
@@ -145,13 +160,14 @@ namespace libecs
 	aVariable->loadValue( theValueBuffer[ c ] );
 
 	//// x(n+1) = x(n) + k2 * aStepInterval + O(h^3)
-	aVariable->setVelocity( theVelocityBuffer[ c ] );
+	aVariable->setVelocity( anExpectedVelocity );
       }
     
     setMaxErrorRatio( maxError );
 
     // reset the stepper current time
     setCurrentTime( aCurrentTime );
+    resetAll();
 
     if ( maxError > 1.1 )
       {
