609cabd99a0e4782ed482480f16e19472aa2ccb3
shafi <shafi@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2004-06-29 16:01:45 +0000

    some code tunings
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@2158 f1531174-cb10-0410-9fe6-89aa7ac3eedb

--- ecell/dm/ExpressionProcessBase.hpp
+++ ecell/dm/ExpressionProcessBase.hpp
@@ -68,10 +68,6 @@ namespace libecs
 
     class StackMachine
     {
-      //      typedef boost::variant< Real, 
-      //			      Pointer,
-      //			      Integer > StackElement_;
-
       union StackElement_
       {
 	Real theReal;
@@ -92,24 +88,9 @@ namespace libecs
     
       ~StackMachine() {}
     
-      void resize( Stack::size_type aSize )
-      {
-	theStack.resize( aSize );
-      }
-    
-      void reset()
-      {
-	theStackPtr = &theStack[0];
-        theStack[0].theReal = 0.0;
-      }
-
       const Real execute( CodeCref aCode );
     
-    protected:
-    
-      Stack           theStack;
-    
-      StackElementPtr theStackPtr;
+
     };
   
   
@@ -165,7 +146,7 @@ namespace libecs
 	theCompiledCode.clear();
 	theCompiledCode = theCompiler.compileExpression( theExpression );
 
-	theStackMachine.resize( theCompiler.getStackSize() );
+	//	theStackMachine.resize( theCompiler.getStackSize() );
 
 	theNeedRecompile = false;
       }
@@ -199,14 +180,8 @@ namespace libecs
 
 
   
-  const Real ExpressionProcessBase::StackMachine::
-  execute( CodeCref aCode )
+  const Real ExpressionProcessBase::StackMachine::execute( CodeCref aCode )
   {
-
-    const char* aPC( &aCode[0] );
-
-    reset();
-
 
 #define FETCH_INSTRUCTION( PC )\
     const ExpressionCompiler::InstructionHead* anInstructionHead\
@@ -215,20 +190,26 @@ namespace libecs
 #define DECODE_INSTRUCTION( OPCODE )\
     typedef ExpressionCompiler::\
       Opcode2Instruction<ExpressionCompiler::OPCODE>::type CurrentInstruction;\
-    const size_t SizeOfCurrentInstruction( sizeof( CurrentInstruction ) );\
     const CurrentInstruction*\
        anInstruction( reinterpret_cast< const CurrentInstruction* >\
       ( anInstructionHead ) );
 
-#define INCREMENT_PC( PC )\
-    PC += SizeOfCurrentInstruction;
+    //std::cout << #OPCODE << std::endl;\
+
+#define INCREMENT_PC( OPCODE )\
+    aPC += sizeof( ExpressionCompiler::\
+                   Opcode2Instruction<ExpressionCompiler::OPCODE>::type );
+
+
+    StackElement    aStack[100];
+    StackElementPtr aStackPtr( aStack );
+    aStackPtr->theReal = 0.0;
+
+    const char* aPC( &aCode[0] );
 
 
     while( 1 )
       {
-	// decode opcode
-	//	const ExpressionCompiler::Opcode* 
-	//anOpcode( (ExpressionCompiler::Opcode*)aPC );
 	FETCH_INSTRUCTION( aPC );
 
 	const ExpressionCompiler::Opcode 
@@ -238,404 +219,307 @@ namespace libecs
 	  {
 	  case ExpressionCompiler::PUSH_REAL:
 	    {
-	      //std::cout << "PUSH_REAL" << std::endl;
-
               DECODE_INSTRUCTION( PUSH_REAL );
 
-	      ++theStackPtr;
-	      theStackPtr->theReal = anInstruction->getOperand();
+	      ++aStackPtr;
+	      aStackPtr->theReal = anInstruction->getOperand();
 	      
-	      INCREMENT_PC( aPC );
+	      INCREMENT_PC( PUSH_REAL );
 	      break;
 	    }
 	    
 	  case ExpressionCompiler::PUSH_INTEGER:
 	    {
-	      //std::cout << "PUSH_INTEGER" << std::endl;
-
               DECODE_INSTRUCTION( PUSH_INTEGER );
 
-	      ++theStackPtr;
-	      theStackPtr->theInteger = anInstruction->getOperand();
+	      ++aStackPtr;
+	      aStackPtr->theInteger = anInstruction->getOperand();
 	      
-	      
-	      INCREMENT_PC( aPC );
+	      INCREMENT_PC( PUSH_INTEGER );
 	      break;
 	    }
 	    
 	  case ExpressionCompiler::PUSH_POINTER:
 	    {
-	      //std::cout << "PUSH_POINTER" << std::endl;
-
               DECODE_INSTRUCTION( PUSH_POINTER );
 
-	      ++theStackPtr;
-	      theStackPtr->thePointer = anInstruction->getOperand();
+	      ++aStackPtr;
+	      aStackPtr->thePointer = anInstruction->getOperand();
 	      
-	      INCREMENT_PC( aPC );
+	      INCREMENT_PC( PUSH_POINTER );
 	      break;
 	    }
 	    
 	  case ExpressionCompiler::NEG:
 	    {
-	      //std::cout << "NEG" << std::endl;
+	      aStackPtr->theReal = - aStackPtr->theReal;
 
-              DECODE_INSTRUCTION( NEG );
-
-	      theStackPtr->theReal = - theStackPtr->theReal;
-
-	      INCREMENT_PC( aPC );
+	      INCREMENT_PC( NEG );
 	      break;
 	    }
 
 	  case ExpressionCompiler::ADD:
 	    {
-	      //std::cout << "ADD" << std::endl;
+	      Real aStackTopValue( aStackPtr->theReal );
+	      --aStackPtr;
 
-              DECODE_INSTRUCTION( ADD );
+	      aStackPtr->theReal += aStackTopValue;
 
-	      Real* aStackTop( &theStackPtr->theReal );
-	      --theStackPtr;
-
-	      theStackPtr->theReal += *aStackTop;
-
-	      INCREMENT_PC( aPC );
+	      INCREMENT_PC( ADD );
 	      break;
 	    }
 
 	  case ExpressionCompiler::SUB:
 	    {
-	      //std::cout << "SUB" << std::endl;
-
-              DECODE_INSTRUCTION( SUB );
-
-	      Real* aStackTop( &theStackPtr->theReal );
-	      --theStackPtr;
+	      Real aStackTopValue( aStackPtr->theReal );
+	      --aStackPtr;
 	      
-	      theStackPtr->theReal -= *aStackTop;
+	      aStackPtr->theReal -= aStackTopValue;
 
-	      INCREMENT_PC( aPC );
+	      INCREMENT_PC( SUB );
 	      break;
 	    }
 
+
 	  case ExpressionCompiler::MUL:
 	    {
-	      //std::cout << "MUL" << std::endl;
+	      Real aStackTopValue( aStackPtr->theReal );
+	      --aStackPtr;
 
-              DECODE_INSTRUCTION( MUL );
+	      aStackPtr->theReal *= aStackTopValue;
 
-	      Real* aStackTop( &theStackPtr->theReal );
-	      --theStackPtr;
-
-	      theStackPtr->theReal *= *aStackTop;
-
-
-	      INCREMENT_PC( aPC );
+	      INCREMENT_PC( MUL );
 	      break;
 	    }
 
 	  case ExpressionCompiler::DIV:
 	    {
-	      //std::cout << "DIV" << std::endl;
-
-              DECODE_INSTRUCTION( DIV );
-
-	      Real* aStackTop( &theStackPtr->theReal );
-	      --theStackPtr;
+	      Real aStackTopValue( aStackPtr->theReal );
+	      --aStackPtr;
 	      
-	      theStackPtr->theReal /= *aStackTop;
+	      aStackPtr->theReal /= aStackTopValue;
 
-
-	      INCREMENT_PC( aPC );
+	      INCREMENT_PC( DIV );
 	      break;
 	    }
 
 	  case ExpressionCompiler::POW:
 	    {
-	      //std::cout << "POW" << std::endl;
+	      Real aStackTopValue( aStackPtr->theReal );
+	      --aStackPtr;
 
-              DECODE_INSTRUCTION( POW );
+	      aStackPtr->theReal = pow( aStackPtr->theReal, aStackTopValue );
 
-	      Real* aStackTop( &theStackPtr->theReal );
-	      --theStackPtr;
-
-	      theStackPtr->theReal = pow( theStackPtr->theReal, *aStackTop );
-
-
-	      INCREMENT_PC( aPC );
+	      INCREMENT_PC( POW );
 	      break;
 	    }
 
 	  case ExpressionCompiler::EQ:
 	    {
-	      //std::cout << "EQ" << std::endl;
-
-              DECODE_INSTRUCTION( EQ );
-
-	      Real* aStackTop( &theStackPtr->theReal );
-	      --theStackPtr;
+	      Real aStackTopValue( aStackPtr->theReal );
+	      --aStackPtr;
 	      
-	      theStackPtr->theReal =
-		Real( *aStackTop == theStackPtr->theReal );
+	      aStackPtr->theReal =
+		Real( aStackTopValue == aStackPtr->theReal );
 
-
-	      INCREMENT_PC( aPC );
+	      INCREMENT_PC( EQ );
 	      break;
 	    }
 
 	  case ExpressionCompiler::NEQ:
 	    {
-	      //std::cout << "NEQ" << std::endl;
-
-              DECODE_INSTRUCTION( NEQ );
-
-	      Real* aStackTop( &theStackPtr->theReal );
-	      --theStackPtr;
+	      Real aStackTopValue( aStackPtr->theReal );
+	      --aStackPtr;
 	      
-	      theStackPtr->theReal =
-		Real( theStackPtr->theReal != *aStackTop );
+	      aStackPtr->theReal =
+		Real( aStackPtr->theReal != aStackTopValue );
 
-
-	      INCREMENT_PC( aPC );
+	      INCREMENT_PC( NEQ );
 	      break;
 	    }
 
 	  case ExpressionCompiler::GT:
 	    {
-	      //std::cout << "GT" << std::endl;
-
-              DECODE_INSTRUCTION( GT );
-
-	      Real* aStackTop( &theStackPtr->theReal );
-	      --theStackPtr;
+	      Real aStackTopValue( aStackPtr->theReal );
+	      --aStackPtr;
 	      
-	      theStackPtr->theReal = Real( theStackPtr->theReal > *aStackTop );
+	      aStackPtr->theReal = Real( aStackPtr->theReal > aStackTopValue );
 
-
-	      INCREMENT_PC( aPC );
+	      INCREMENT_PC( GT );
 	      break;
 	    }
 
 	  case ExpressionCompiler::GEQ:
 	    {
-	      //std::cout << "GEQ" << std::endl;
-
-              DECODE_INSTRUCTION( GEQ );
-
-	      Real* aStackTop( &theStackPtr->theReal );
-	      --theStackPtr;
+	      Real aStackTopValue( aStackPtr->theReal );
+	      --aStackPtr;
 	      
-	      theStackPtr->theReal =
-		Real( theStackPtr->theReal >= *aStackTop );
+	      aStackPtr->theReal =
+		Real( aStackPtr->theReal >= aStackTopValue );
 
-
-	      INCREMENT_PC( aPC );
+	      INCREMENT_PC( GEQ );
 	      break;
 	    }
 
 	  case ExpressionCompiler::LT:
 	    {
-	      //std::cout << "LT" << std::endl;
-
-              DECODE_INSTRUCTION( LT );
-
-	      Real* aStackTop( &theStackPtr->theReal );
-	      --theStackPtr;
+	      Real aStackTopValue( aStackPtr->theReal );
+	      --aStackPtr;
 	      
-	      theStackPtr->theReal = Real( theStackPtr->theReal < *aStackTop );
+	      aStackPtr->theReal = Real( aStackPtr->theReal < aStackTopValue );
 
-
-	      INCREMENT_PC( aPC );
+	      INCREMENT_PC( LT );
 	      break;
 	    }
 
 	  case ExpressionCompiler::LEQ:
 	    {
-	      //std::cout << "LEQ" << std::endl;
-
-              DECODE_INSTRUCTION( LEQ );
-
-	      Real* aStackTop( &theStackPtr->theReal );
-	      --theStackPtr;
+	      Real aStackTopValue( aStackPtr->theReal );
+	      --aStackPtr;
 	      
-	      theStackPtr->theReal =
-		Real( theStackPtr->theReal <= *aStackTop );
+	      aStackPtr->theReal =
+		Real( aStackPtr->theReal <= aStackTopValue );
 
-
-	      INCREMENT_PC( aPC );
+	      INCREMENT_PC( LEQ );
 	      break;
 	    }
 
 	  case ExpressionCompiler::AND:
 	    {
-	      //std::cout << "AND" << std::endl;
-
-              DECODE_INSTRUCTION( AND );
-
-	      Real* aStackTop( &theStackPtr->theReal );
-	      --theStackPtr;
+	      Real aStackTopValue( aStackPtr->theReal );
+	      --aStackPtr;
 	      
-	      theStackPtr->theReal =
-		Real( *aStackTop && theStackPtr->theReal );
+	      aStackPtr->theReal =
+		Real( aStackTopValue && aStackPtr->theReal );
 
-
-	      INCREMENT_PC( aPC );
+	      INCREMENT_PC( AND );
 	      break;
 	    }
 
 	  case ExpressionCompiler::OR:
 	    {
-	      //std::cout << "OR" << std::endl;
-
-              DECODE_INSTRUCTION( OR );
-
-	      Real* aStackTop( &theStackPtr->theReal );
-	      --theStackPtr;
+	      Real aStackTopValue( aStackPtr->theReal );
+	      --aStackPtr;
 	      
-	      theStackPtr->theReal = 
-		Real( *aStackTop || theStackPtr->theReal );
+	      aStackPtr->theReal = 
+		Real( aStackTopValue || aStackPtr->theReal );
 
-
-	      INCREMENT_PC( aPC );
+	      INCREMENT_PC( OR );
 	      break;
 	    }
 
 	  case ExpressionCompiler::XOR:
 	    {
-	      //std::cout << "XOR" << std::endl;
-
-              DECODE_INSTRUCTION( XOR );
-
-	      Real* aStackTop( &theStackPtr->theReal );
-	      --theStackPtr;
+	      Real aStackTopValue( aStackPtr->theReal );
+	      --aStackPtr;
 	      
-	      theStackPtr->theReal = 
-		Real( theStackPtr->theReal && !( *aStackTop ) );
+	      aStackPtr->theReal = 
+		Real( aStackPtr->theReal && !( aStackTopValue ) );
 
-
-	      INCREMENT_PC( aPC );
+	      INCREMENT_PC( XOR );
 	      break;
 	    }
 
 	  case ExpressionCompiler::NOT:
 	    {
-	      //std::cout << "NOT" << std::endl;
+	      aStackPtr->theReal = !( aStackPtr->theReal );
 
-              DECODE_INSTRUCTION( NOT );
-
-	      theStackPtr->theReal = !( theStackPtr->theReal );
-
-	      INCREMENT_PC( aPC );
+	      INCREMENT_PC( NOT );
 	      break;
 	    }
 
 	  case ExpressionCompiler::CALL_FUNC1:
 	    {
-	      //std::cout << "CALL_FUNC1" << std::endl;
-
               DECODE_INSTRUCTION( CALL_FUNC1 );
 
-	      theStackPtr->theReal
-		= ( anInstruction->getOperand() )( theStackPtr->theReal );
+	      aStackPtr->theReal
+		= ( anInstruction->getOperand() )( aStackPtr->theReal );
 	      
-	      INCREMENT_PC( aPC );
+	      INCREMENT_PC( CALL_FUNC1 );
 	      break;
 	    }
 
 	  case ExpressionCompiler::CALL_FUNC2:
 	    {
-	      //std::cout << "CALL_FUNC2" << std::endl;
-
               DECODE_INSTRUCTION( CALL_FUNC2 );
 	      
-	      Real* aStackTop( &theStackPtr->theReal );
-	      --theStackPtr;
+	      Real aStackTopValue( aStackPtr->theReal );
+	      --aStackPtr;
 
-	      theStackPtr->theReal
-		= ( anInstruction->getOperand() )( theStackPtr->theReal, *aStackTop );
+	      aStackPtr->theReal
+		= ( anInstruction->getOperand() )( aStackPtr->theReal, aStackTopValue );
 
 
-	      INCREMENT_PC( aPC );
+	      INCREMENT_PC( CALL_FUNC2 );
 	      break;
 	    }
 	  
 	  case ExpressionCompiler::LOAD_REAL:
 	    {
-	      //std::cout << "LOAD_REAL" << std::endl;
-
 	      DECODE_INSTRUCTION( LOAD_REAL );
 	      
-	      ++theStackPtr;
-	      theStackPtr->theReal = *( anInstruction->getOperand() );
+	      ++aStackPtr;
+	      aStackPtr->theReal = *( anInstruction->getOperand() );
 	      
-	      INCREMENT_PC( aPC );
+	      INCREMENT_PC( LOAD_REAL );
 	      break;
 	    }
 
 	  case ExpressionCompiler::VARREF_METHOD:
 	    {
-	      //std::cout << "VARREF_METHOD" << std::endl;
-
 	      DECODE_INSTRUCTION( VARREF_METHOD );
 	      
 	      ExpressionCompiler::VariableReferenceMethod
 		aVariableReferenceMethod( anInstruction->getOperand() );
 
-	      ++theStackPtr;
-	      theStackPtr->theReal =
+	      ++aStackPtr;
+	      aStackPtr->theReal =
 		( ( *( aVariableReferenceMethod.theOperand1 ) ).*( aVariableReferenceMethod.theOperand2 ) )();
 
-	      INCREMENT_PC( aPC );
+	      INCREMENT_PC( VARREF_METHOD );
 	      break;
 	    }
 
 	  case ExpressionCompiler::PROCESS_TO_SYSTEM_METHOD:
 	    {
-	      //std::cout << "PROCESS_TO_SYSTEM_METHOD" << std::endl;
-
 	      DECODE_INSTRUCTION( PROCESS_TO_SYSTEM_METHOD );
 
-	      reinterpret_cast<System*>( theStackPtr->thePointer ) =
-		( *( reinterpret_cast<Process*>( theStackPtr->thePointer ) ).*( anInstruction->getOperand() ) )();
+	      reinterpret_cast<System*>( aStackPtr->thePointer ) =
+		( *( reinterpret_cast<Process*>( aStackPtr->thePointer ) ).*( anInstruction->getOperand() ) )();
 
-	      INCREMENT_PC( aPC );
+	      INCREMENT_PC( PROCESS_TO_SYSTEM_METHOD );
 	      break;
 	    }
 
 	  case ExpressionCompiler::VARREF_TO_SYSTEM_METHOD:
 	    {
-	      //std::cout << "VARREF_TO_SYSTEM_METHOD" << std::endl;
-
 	      DECODE_INSTRUCTION( VARREF_TO_SYSTEM_METHOD );
 
-	      reinterpret_cast<System*>( theStackPtr->thePointer ) =
-		( *( reinterpret_cast<VariableReference*>( theStackPtr->thePointer ) ).*( anInstruction->getOperand() ) )();
+	      reinterpret_cast<System*>( aStackPtr->thePointer ) =
+		( *( reinterpret_cast<VariableReference*>
+		     ( aStackPtr->thePointer ) ).*( anInstruction->getOperand() ) )();
 
-	      INCREMENT_PC( aPC );
+	      INCREMENT_PC( VARREF_TO_SYSTEM_METHOD );
 	      break;
 	    }
 
 	  case ExpressionCompiler::SYSTEM_TO_REAL_METHOD:
 	    {
-	      //std::cout << "SYSTEM_TO_REAL_METHOD" << std::endl;
-
 	      DECODE_INSTRUCTION( SYSTEM_TO_REAL_METHOD );
 
-	      theStackPtr->theReal =
-		( *( reinterpret_cast<System*>( theStackPtr->thePointer ) ).*( anInstruction->getOperand() ) )();
+	      aStackPtr->theReal =
+		( *( reinterpret_cast<System*>( aStackPtr->thePointer ) ).*( anInstruction->getOperand() ) )();
 
-	      INCREMENT_PC( aPC );
+	      INCREMENT_PC( SYSTEM_TO_REAL_METHOD );
 	      break;
 	    }
 
 	  case ExpressionCompiler::HALT:
 	    {
-	      //std::cout << "HALT" << std::endl;
+              //DECODE_INSTRUCTION( HALT );
+	      //INCREMENT_PC( HALT );
 
-              DECODE_INSTRUCTION( HALT );
-
-	      INCREMENT_PC( aPC );
-	      return theStackPtr->theReal;
+	      return aStackPtr->theReal;
 	    }
 
 	  default:
