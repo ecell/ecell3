919837e0a66aaeba062909a77f910e9762f6640b
moriyoshi <moriyoshi@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2007-05-16 05:41:35 +0000

    * Use INF constant where possible.
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@2688 f1531174-cb10-0410-9fe6-89aa7ac3eedb

--- ecell/dm/FluxDistributionStepper.cpp
+++ ecell/dm/FluxDistributionStepper.cpp
@@ -45,12 +45,7 @@ FluxDistributionStepper::FluxDistributionStepper()
   theIrreversibleFlag( false ),
   Epsilon( 1e-6 )
 {
-  // gcc3 doesn't currently support numeric_limits::infinity.
-  // using max() instead.
-  const Real anInfinity( std::numeric_limits<Real>::max() *
-			 std::numeric_limits<Real>::max() );
-
-  initializeStepInterval( anInfinity );
+  initializeStepInterval( INF );
 }
 
 FluxDistributionStepper::~FluxDistributionStepper()
