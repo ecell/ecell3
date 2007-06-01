7ced4a28a7c0f47830cd468cb2cac113a904beea
moriyoshi <moriyoshi@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2007-06-01 06:33:12 +0000

    * DLFCN is not available on every platform...
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@2728 f1531174-cb10-0410-9fe6-89aa7ac3eedb

--- ecell/pyecs/ecell/ecsinit.py
+++ ecell/pyecs/ecell/ecsinit.py
@@ -2,11 +2,13 @@ import os
 
 if os.name != "nt":
     import sys
-    import DLFCN
-    
-    # RTLD_GLOBAL is needed so that rtti across dynamic modules can work
-    # RTLD_LAZY   may be needed so that the system can resolve dependency among
-    #             dynamic modules after dlopened it
-    
-    sys.setdlopenflags( DLFCN.RTLD_LAZY | DLFCN.RTLD_GLOBAL )
-
+    try:
+        import DLFCN
+        
+        # RTLD_GLOBAL is needed so that rtti across dynamic modules can work
+        # RTLD_LAZY   may be needed so that the system can resolve dependency among
+        #             dynamic modules after dlopened it
+        
+        sys.setdlopenflags( DLFCN.RTLD_LAZY | DLFCN.RTLD_GLOBAL )
+    except:
+        None
