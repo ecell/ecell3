f4924fede4df794b2f7a5fa381ead9309afc3f03
moriyoshi <moriyoshi@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2007-06-06 12:43:40 +0000

    * Header update (python sources)
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@2765 f1531174-cb10-0410-9fe6-89aa7ac3eedb

--- ecell/model-editor/ConfirmWindow.py
+++ ecell/model-editor/ConfirmWindow.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python
-
 #::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 #
-#        This file is part of E-CELL Session Monitor package
+#       This file is part of the E-Cell System
 #
-#                Copyright (C) 1996-2002 Keio University
+#       Copyright (C) 1996-2007 Keio University
+#       Copyright (C) 2005-2007 The Molecular Sciences Institute
 #
 #::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 #
 #
-# E-CELL is free software; you can redistribute it and/or
+# E-Cell System is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public
 # License as published by the Free Software Foundation; either
 # version 2 of the License, or (at your option) any later version.
 # 
-# E-CELL is distributed in the hope that it will be useful,
+# E-Cell System is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the GNU General Public License for more details.
 # 
 # You should have received a copy of the GNU General Public
-# License along with E-CELL -- see the file COPYING.
+# License along with E-Cell System -- see the file COPYING.
 # If not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 # 
