361f8804da4c8aab7a62df01fca43c82c69e88b1
moriyoshi <moriyoshi@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2007-06-27 09:13:23 +0000

    * "from __future__ import blah" must come first .
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/trunk@2815 f1531174-cb10-0410-9fe6-89aa7ac3eedb

--- ecell/model-editor/ecell/ui/model_editor/LineDescriptor.py
+++ ecell/model-editor/ecell/ui/model_editor/LineDescriptor.py
@@ -24,8 +24,8 @@
 # 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 # 
 #END_HEADER
-from Constants import *
 from __future__ import division
+from Constants import *
 import numpy as nu
 try:
     import gnomecanvas
