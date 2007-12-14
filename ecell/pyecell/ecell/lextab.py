8c55f5a3253ebaf7ca28cf8e1b0bf9bb97d133b3
moriyoshi <moriyoshi@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2007-12-14 13:32:45 +0000

    * This file is not part of the E-Cell System ;p
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/branches/ecell-3.1@2905 f1531174-cb10-0410-9fe6-89aa7ac3eedb

--- ecell/pyecell/ecell/lextab.py
+++ ecell/pyecell/ecell/lextab.py
@@ -1,29 +1,3 @@
-#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-#
-#       This file is part of the E-Cell System
-#
-#       Copyright (C) 1996-2007 Keio University
-#       Copyright (C) 2005-2007 The Molecular Sciences Institute
-#
-#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-#
-#
-# E-Cell System is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public
-# License as published by the Free Software Foundation; either
-# version 2 of the License, or (at your option) any later version.
-# 
-# E-Cell System is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public
-# License along with E-Cell System -- see the file COPYING.
-# If not, write to the Free Software Foundation, Inc.,
-# 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-# 
-#END_HEADER
 # lextab.py.  This file automatically created by PLY. Don't edit.
 _lexre = '(?P<t_Stepper> Stepper[\\s|\\t] )|(?P<t_System> System[\\s|\\t] )|(?P<t_Process> Process[\\s|\\t] )|(?P<t_Variable> Variable[\\s|\\t] )|(?P<t_LPAREN>\\()|(?P<t_RPAREN>\\))|(?P<t_LBRACKET>\\[)|(?P<t_RBRACKET>\\])|(?P<t_LBRACE>\\{)|(?P<t_RBRACE>\\})|(?P<t_SEMI>;)|(?P<t_number> [+-]?(\\d+(\\.\\d*)?|\\d*\\.\\d+)([eE][+-]?\\d+)? )|(?P<t_name>[a-zA-Z_/][\\w\\:\\/.]*)|(?P<t_quotedstring> "(^"|.)*" | \\\'(^\\\'|.)*\\\' )|(?P<t_control> \\%line [^\\n]*\\n )|(?P<t_comment> \\# [^\\n]* )|(?P<t_nl> \\n )|(?P<t_whitespace> [ |\\t]+ )|(?P<t_default> .+ )'
 _lextab = [
