f1f732db64d2e453feef7218ce754ff0c0f50541
moriyoshi <moriyoshi@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2008-10-20 08:37:54 +0000

    * Invoke setDMSearchPath() in the constructor for backwards compatibility.
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/branches/ecell-3.1@3229 f1531174-cb10-0410-9fe6-89aa7ac3eedb

--- ecell/pyecell/ecell/ecs.py
+++ ecell/pyecell/ecell/ecs.py
@@ -24,19 +24,42 @@
 # 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 # 
 #END_HEADER
-import os
 
-if os.name != "nt":
-    import sys
-    try:
-        import DLFCN
-        
-        # RTLD_GLOBAL is needed so that rtti across dynamic modules can work
-        # RTLD_LAZY   may be needed so that the system can resolve dependency among
-        #             dynamic modules after dlopened it
-        
-        sys.setdlopenflags( DLFCN.RTLD_LAZY | DLFCN.RTLD_GLOBAL )
-    except:
-        None
+import ecell._ecs
 
-from ecell._ecs import *
+__all__ = (
+    'getLibECSVersionInfo',
+    'getLibECSVersion',
+    'VariableReference',
+    'Process',
+    'System',
+    'VariableReferenceVector',
+    'Simulator'
+    )
+
+def __init__():
+    import os
+    if os.name != "nt":
+        import sys
+        try:
+            import DLFCN
+            
+            # RTLD_GLOBAL is needed so that rtti across dynamic modules can work
+            # RTLD_LAZY   may be needed so that the system can resolve dependency among
+            #             dynamic modules after dlopened it
+            
+            sys.setdlopenflags( DLFCN.RTLD_LAZY | DLFCN.RTLD_GLOBAL )
+        except:
+            None
+
+    for i in __all__:
+        globals()[ i ] = getattr( ecell._ecs, i )
+
+__init__()
+
+class Simulator( ecell._ecs.Simulator ):
+    def __init__( self, *args, **kwargs ):
+        import ecell.config
+        ecell._ecs.Simulator.__init__( self, *args, **kwargs )
+        self.setDMSearchPath( self.DMSearchPathSeparator.join( ecell.config.dm_path ) )
+
