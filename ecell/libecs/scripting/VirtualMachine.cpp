6042f060fcb5b2d36be148ab4b6356b8f1f1ac33
moriyoshi <moriyoshi@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2009-01-04 19:10:08 +0000

    * I don't know why std::vector<>.data() worked so far...
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/branches/ecell-3.2@3349 f1531174-cb10-0410-9fe6-89aa7ac3eedb

--- ecell/libecs/scripting/VirtualMachine.cpp
+++ ecell/libecs/scripting/VirtualMachine.cpp
@@ -32,9 +32,9 @@
 // E-Cell Project.
 //
 
-#include "libecs.hpp"
-#include "Exceptions.hpp"
-#include "scripting/VirtualMachine.hpp"
+#include "libecs/libecs.hpp"
+#include "libecs/Exceptions.hpp"
+#include "libecs/scripting/VirtualMachine.hpp"
 
 namespace libecs { namespace scripting {
 using namespace libecs;
@@ -153,7 +153,7 @@ const Real VirtualMachine::execute( CodeCref aCode )
  
     LightweightStack<StackElement, 100> aStack;
 
-    const unsigned char* aPC( aCode.data() );
+    const unsigned char* aPC( &aCode.front() );
 
     for (;;) {
         switch ( FETCH_OPCODE() ) {
