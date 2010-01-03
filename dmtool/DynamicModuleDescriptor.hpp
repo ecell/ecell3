34423b9ebcc75ebb554370eb0e50ba6de573d0d7
moriyoshi <moriyoshi@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2010-01-03 02:25:31 +0000

    * Bumping year.
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/branches/ecell-3.2@3644 f1531174-cb10-0410-9fe6-89aa7ac3eedb

--- dmtool/DynamicModuleDescriptor.hpp
+++ dmtool/DynamicModuleDescriptor.hpp
@@ -2,8 +2,8 @@
 //
 //       This file is part of the E-Cell System
 //
-//       Copyright (C) 1996-2009 Keio University
-//       Copyright (C) 2005-2008 The Molecular Sciences Institute
+//       Copyright (C) 1996-2010 Keio University
+//       Copyright (C) 2005-2009 The Molecular Sciences Institute
 //
 //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 //
