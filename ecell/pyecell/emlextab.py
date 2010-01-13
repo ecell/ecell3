c2bed5ef9d2b452e24f7d5770d48061c956dcc2f
moriyoshi <moriyoshi@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2010-01-13 03:22:49 +0000

    * Generate lextab and parsetab during the buld.
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/branches/ecell-3.2@3681 f1531174-cb10-0410-9fe6-89aa7ac3eedb

--- ecell/pyecell/emlextab.py
+++
@@ -1,9 +0,0 @@
-# ecell.emlextab.py. This file automatically created by PLY (version 3.2). Don't edit!
-_tabversion   = '3.2'
-_lextokens    = {'LBRACE': 1, 'systempath': 1, 'LPAREN': 1, 'SEMI': 1, 'Process': 1, 'Stepper': 1, 'System': 1, 'quotedstring': 1, 'number': 1, 'LBRACKET': 1, 'fullid': 1, 'quotedstrings': 1, 'Variable': 1, 'RPAREN': 1, 'RBRACKET': 1, 'identifier': 1, 'RBRACE': 1}
-_lexreflags   = 0
-_lexliterals  = ''
-_lexstateinfo = {'INITIAL': 'inclusive'}
-_lexstatere   = {'INITIAL': [('(?P<t_Stepper> Stepper[\\s|\\t] )|(?P<t_System> System[\\s|\\t] )|(?P<t_Process> Process[\\s|\\t] )|(?P<t_Variable> Variable[\\s|\\t] )|(?P<t_number> [+-]?(\\d+(\\.\\d*)?|\\d*\\.\\d+)([eE][+-]?\\d+)? )|(?P<t_fullid>[a-zA-Z]*:[\\w/\\.]*:\\w*)|(?P<t_identifier>[a-zA-Z_][a-zA-Z0-9_]*)|(?P<t_systempath>[a-zA-Z_/\\.]+[\\w/\\.]*)|(?P<t_quotedstrings> """[^"]*""" | \\\'\\\'\\\'[^\\\']*\\\'\\\'\\\' )|(?P<t_quotedstring> "(^"|.)*" | \\\'(^\\\'|.)*\\\' )|(?P<t_control> \\%line\\s[^\\n]*\\n )|(?P<t_comment> \\#[^\\n]* )|(?P<t_nl> \\n+ )|(?P<t_whitespace> [ |\\t]+ )|(?P<t_RBRACE>\\})|(?P<t_LBRACE>\\{)|(?P<t_RBRACKET>\\])|(?P<t_RPAREN>\\))|(?P<t_LBRACKET>\\[)|(?P<t_LPAREN>\\()|(?P<t_SEMI>;)', [None, ('t_Stepper', 'Stepper'), ('t_System', 'System'), ('t_Process', 'Process'), ('t_Variable', 'Variable'), ('t_number', 'number'), None, None, None, ('t_fullid', 'fullid'), ('t_identifier', 'identifier'), ('t_systempath', 'systempath'), ('t_quotedstrings', 'quotedstrings'), ('t_quotedstring', 'quotedstring'), None, None, ('t_control', 'control'), ('t_comment', 'comment'), ('t_nl', 'nl'), ('t_whitespace', 'whitespace'), (None, 'RBRACE'), (None, 'LBRACE'), (None, 'RBRACKET'), (None, 'RPAREN'), (None, 'LBRACKET'), (None, 'LPAREN'), (None, 'SEMI')])]}
-_lexstateignore = {'INITIAL': ''}
-_lexstateerrorf = {'INITIAL': 't_error'}
