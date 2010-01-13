a8e1e5f6c78cd8212dbf4f3e19265f6efe2966d9
moriyoshi <moriyoshi@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2010-01-13 08:24:24 +0000

    * Oops.
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/branches/ecell-3.2@3691 f1531174-cb10-0410-9fe6-89aa7ac3eedb

--- ecell/frontend/model-editor/ecell/ui/model_editor/glade_compat.py
+++ ecell/frontend/model-editor/ecell/ui/model_editor/glade_compat.py
@@ -10,7 +10,7 @@
 #
 # E-Cell System is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public
-#print  License as published by the Free Software Foundation; either
+# License as published by the Free Software Foundation; either
 # version 2 of the License, or (at your option) any later version.
 # 
 # E-Cell System is distributed in the hope that it will be useful,
