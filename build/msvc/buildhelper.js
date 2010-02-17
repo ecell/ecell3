ed9c78e296045b9ee9a37b8c6e0874ab892e2185
moriyoshi <moriyoshi@f1531174-cb10-0410-9fe6-89aa7ac3eedb>
2010-02-17 17:39:43 +0000

    * Merge from trunk.
    git-svn-id: https://ecell.svn.sourceforge.net/svnroot/ecell/ecell3/branches/ecell-3.1@3826 f1531174-cb10-0410-9fe6-89aa7ac3eedb

--- build/msvc/buildhelper.js
+++ build/msvc/buildhelper.js
@@ -107,7 +107,7 @@ BuildHelper.prototype = {
         WScript.Echo("Creating launcher script " + script_file_name);
         var scr = '';
         scr += '@ECHO OFF\n';
-        scr += 'SET PWD=%~dp0\n';
+        scr += 'SET PWD=%~dps0\n';
         scr += 'IF "%ECELL_HOME%" == "" SET ECELL_HOME=%PWD%\\..\n';
         scr += 'SET PYTHONPATH=%PYTHONPATH%;%ECELL_HOME%\\lib\\site-packages\n';
         scr += 'SET PATH=%PWD%;%PATH%\n';
